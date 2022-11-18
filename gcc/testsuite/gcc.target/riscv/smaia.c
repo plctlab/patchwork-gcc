ee63a8da4bd8b82d7f41f4381b8ed6dea3f7d2e5
Christoph Müllner <christoph.muellner@vrull.eu>
2022-11-18 03:12:23 +0100

    RISC-V: Add support for AIA ISA extensions (Ssaia and Smaia)
    This patch adds support for the two AIA ISA extensions Ssaia and Smaia.
    They are not relelvant for the compiler, but the assembler might want
    to validate the CSRs. Therefore, all this patch does is recognize the
    extension name, emit a feature macro (incl. a test).
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

---
+++ gcc/testsuite/gcc.target/riscv/smaia.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gc_smaia" { target { rv64 } } } */
+/* { dg-options "-march=rv32gc_smaia" { target { rv32 } } } */
+
+#ifndef __riscv_smaia
+#error Feature macro not defined
+#endif
+
+int
+foo (int a)
+{
+  return a;
+}
