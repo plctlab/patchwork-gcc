15428d1b0d10198d3b7c61ffc134ad4deaa78e16
Patrick Palka <ppalka@redhat.com>
2022-12-02 14:01:10 -0500

    c++: substituting CONST_DECL_USING_P enumerator [PR103081]
    We implement using enum at class scope by injecting clones of the enum's
    CONST_DECLs as fields of the class, for which CONST_DECL_USING_P is
    true, so that qualified lookup naturally finds the enumerators.
    Substitution into such a CONST_DECL currently ICEs however, because we
    assume the DECL_CONTEXT is always the ENUMERAL_TYPE (which has TYPE_VALUES)
    but in this case it's the RECORD_TYPE of the class scope (which has
    TYPE_FIELDS).
    
    Since these CONST_DECLs appear to always be non-dependent, this patch
    fixes this by shortcutting substitution for CONST_DECLs which have a
    non-dependent scope.  This subsumes the existing (and seemingly dead)
    DECL_NAMESPACE_SCOPE_P early exit test and also benefits substitution
    into ordinary non-dependent CONST_DECLs.
    
    Bootstrapped and regtested on x86_64-pc-linu-xgnu, does this look OK for
    trunk/12?
    
    	PR c++/103081
    
    gcc/cp/ChangeLog:
    
    	* pt.cc (tsubst_copy) <case CONST_DECL>: Generalize
    	early exit test for namespace-scope decls to check dependence of
    	the enclosing scope instead.  Remove dead args early exit test.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp2a/using-enum-10.C: New test.
    	* g++.dg/cpp2a/using-enum-10a.C: New test.

---
+++ gcc/testsuite/g++.dg/cpp2a/using-enum-10a.C
@@ -0,0 +1,18 @@
+// A version of using-enum-10.C where Hog is a template.
+// PR c++/103081
+// { dg-do compile { target c++20 } }
+
+enum class Pig { OINK };
+
+template<int>
+struct Hog {
+  using enum Pig;
+  Hog(Pig) { OINK; }
+};
+
+template<int N>
+void pen() {
+  Hog<N>(Hog<N>::OINK);
+}
+
+template void pen<0>();
