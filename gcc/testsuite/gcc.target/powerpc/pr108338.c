d31cccc4e65160caadddb47249246eb434927f7b
Jiufu Guo <guojiufu@linux.ibm.com>
2023-01-10 21:45:27 +0800

    rs6000: Enhance lowpart/highpart DI->SF by mtvsrws/mtvsrd
    Hi,
    
    As mentioned in PR108338, on p9, we could use mtvsrws to implement
    the conversion from SI#0 to SF (or lowpart DI to SF).  And we find
    we can also enhance the conversion from highpart DI to SF (as the
    case in this patch).
    
    This patch enhances these conversions accordingly.
    
    Bootstrap and regtests pass on ppc64{,le}.
    Is this ok for trunk?
    
    BR,
    Jeff (Jiufu)
    
    	PR target/108338
    
    gcc/ChangeLog:
    
    	* config/rs6000/rs6000.md (any_rshift): New code_iterator.
    	(movsf_from_si2): Rename to...
    	(movsf_from_si2_<any_rshift:code>): ... this.
    	(p9_mtvsrws): New define_insn.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/powerpc/pr108338.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/pr108338.c
@@ -0,0 +1,41 @@
+// { dg-do run }
+// { dg-options "-O2 -save-temps" }
+
+float __attribute__ ((noipa)) sf_from_di_off0 (long long l)
+{
+  char buff[16];
+  *(long long*)buff = l;
+  float f = *(float*)(buff);
+  return f;    
+}
+
+float  __attribute__ ((noipa)) sf_from_di_off4 (long long l)
+{
+  char buff[16];
+  *(long long*)buff = l;
+  float f = *(float*)(buff + 4);
+  return f; 
+}
+
+/* { dg-final { scan-assembler-times {\mmtvsrws\M} 1 { target { has_arch_ppc64 && has_arch_pwr9 } } } } */
+/* { dg-final { scan-assembler-times {\mmtvsrd\M} 1 { target { has_arch_ppc64 && has_arch_pwr9 } } } } */
+/* { dg-final { scan-assembler-times {\mrldicr\M} 1 { target { has_arch_ppc64 && has_arch_pwr9 } } } } */
+/* { dg-final { scan-assembler-times {\mxscvspdpn\M} 2 { target { has_arch_pwr8 && has_arch_ppc64 } } } } */
+
+/* { dg-final { scan-assembler-times {\mmtvsrd\M} 2 { target { has_arch_pwr8 && { has_arch_ppc64 && { ! has_arch_pwr9 } } } } } } */
+
+union di_sf_sf
+{
+  struct {float f1; float f2;};
+  long long l;
+};
+
+int main()
+{
+  union di_sf_sf v;
+  v.f1 = 1.0f;
+  v.f2 = 2.0f;
+  if (sf_from_di_off0 (v.l) != 1.0f || sf_from_di_off4 (v.l) != 2.0f )
+    __builtin_abort ();
+  return 0;
+}
