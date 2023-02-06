98cc4b0378b26ef6d8bec7a63dde43642e553ee7
Tobias Burnus <tobias@codesourcery.com>
2023-02-06 12:52:11 +0100

    libgomp: Fix reverse-offload for GOMP_MAP_TO_PSET
    Seems as if I missed a GOMP_MAP_TO_PSET issue before. As nvptx is
    XFAILed before, I only found it when testing on AMDGCN.
    
    For an array-descriptor 'ai' variable, omplower has:
       map(tofrom:MEM <integer(kind=4)[0:]> [(integer(kind=4)[0:] *)D.4346] [len: D.4345])
       map(to:ai [pointer set, len: 64])
       map(alloc:ai.data [pointer assign, bias: 0])
    
    The latter reaches GCC with the same address as 'ai' – i.e. the
    one of the array descriptor. This then needs to be dereferenced
    to get the address of the actual pointer.
    The patch assumes (and asserts) that 'ai.data' is part of the 'ai'
    such that I can use the host address of 'ai' to access the data.
    If that's not guaranteed, we have to find another way (e.g. another
    lookup). But so far it seems to hold and I have not seen a bias
    other than 0.
    
    With that patch, libgomp.fortran/reverse-offload-5.f90 now works
    with AMDGCN.
    
    OK? Any comments to the attached patch?
    
    Tobias
    -----------------
    Siemens Electronic Design Automation GmbH; Anschrift: Arnulfstraße 201, 80634 München; Gesellschaft mit beschränkter Haftung; Geschäftsführer: Thomas Heurung, Frank Thürauf; Sitz der Gesellschaft: München; Registergericht München, HRB 106955
    
    libgomp: Fix reverse-offload for GOMP_MAP_TO_PSET
    
    libgomp/
    	* target.c (gomp_target_rev): Dereference ptr
    	to get device address.
    	* libgomp.fortran/reverse-offload-5.f90: Add test
    	for unallocated allocatable.
    
     libgomp/target.c                                        | 7 ++++++-
     libgomp/testsuite/libgomp.fortran/reverse-offload-5.f90 | 6 ++++--
     2 files changed, 10 insertions(+), 3 deletions(-)

--- libgomp/target.c
+++ libgomp/target.c
@@ -3579,8 +3579,13 @@ gomp_target_rev (uint64_t fn_ptr, uint64_t mapnum, uint64_t devaddrs_ptr,
 		      }
 		    int k;
 		    n2 = NULL;
-		    cdata[i].present = true;
+		    /* Dereference devaddrs[j] to get the device addr.  */
+		    assert (devaddrs[j]-sizes[j] == cdata[i].devaddr);
+		    devaddrs[j] = *(uint64_t *) (devaddrs[i] + sizes[j]);
+		    cdata[j].present = true;
 		    cdata[j].devaddr = devaddrs[j];
+		    if (devaddrs[j] == 0)
+		      continue;
 		    k = gomp_map_cdata_lookup (cdata, devaddrs, kinds, sizes, j,
 					       devaddrs[j],
 					       devaddrs[j] + sizeof (void*),
