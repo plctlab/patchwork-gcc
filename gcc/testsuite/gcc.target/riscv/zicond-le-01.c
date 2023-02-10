a5e3f66aa70b93fc6eff933a35a15d54d2833855
Philipp Tomsich <philipp.tomsich@vrull.eu>
2023-02-10 23:41:46 +0100

    RISC-V: Recognize sign-extract + and cases for czero.eqz/nez
    Users might use explicit arithmetic operations to create a mask and
    then and it, in a sequence like
        cond = (bits >> SHIFT) & 1;
        mask = ~(cond - 1);
        val &= mask;
    which will present as a single-bit sign-extract.
    
    Dependening on what combination of XVentanaCondOps and Zbs are
    available, this will map to the following sequences:
     - bexti + czero,     if both Zbs and XVentanaCondOps are present
     - andi + czero,      if only XVentanaCondOps is available and the
    		      sign-extract is operating on bits 10:0 (bit 11
    		      can't be reached, as the immediate is
    		      sign-extended)
     - slli + srli + and, otherwise.
    
    gcc/ChangeLog:
    
    	* config/riscv/zicond.md: Recognize SIGN_EXTRACT of a
    	single-bit followed by AND for Zicond.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/zicond-le-01.c: New test.
    
    Signed-off-by: Philipp Tomsich <philipp.tomsich@vrull.eu>

---
+++ gcc/testsuite/gcc.target/riscv/zicond-le-01.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gc_zicond -mabi=lp64 -mbranch-cost=4" } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-Og" "-O1" "-Os" "-Oz"  } } */
+
+long long sink (long long);
+
+long long le1 (long long a, long long b)
+{
+  if (a <= b)
+    b = 0;
+
+  return sink(b);
+}
+
+/* { dg-final { scan-assembler-times "sgt\t" 1 } } */
+/* { dg-final { scan-assembler-times "czero.eqz\t" 1 } } */
