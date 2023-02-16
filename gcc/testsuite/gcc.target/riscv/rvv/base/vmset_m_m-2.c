44af9ecaa782ecdf9ad9cc4195c5e5d66eee3b7e
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-02-16 11:36:19 +0800

    RISC-V: Add vm* mask C api tests
    gcc/testsuite/ChangeLog:
    
            * gcc.target/riscv/rvv/base/vmand_mm-1.c: New test.
            * gcc.target/riscv/rvv/base/vmand_mm-2.c: New test.
            * gcc.target/riscv/rvv/base/vmand_mm-3.c: New test.
            * gcc.target/riscv/rvv/base/vmandn_mm-1.c: New test.
            * gcc.target/riscv/rvv/base/vmandn_mm-2.c: New test.
            * gcc.target/riscv/rvv/base/vmandn_mm-3.c: New test.
            * gcc.target/riscv/rvv/base/vmclr_m_m-1.c: New test.
            * gcc.target/riscv/rvv/base/vmclr_m_m-2.c: New test.
            * gcc.target/riscv/rvv/base/vmclr_m_m-3.c: New test.
            * gcc.target/riscv/rvv/base/vmmv_m_m-1.c: New test.
            * gcc.target/riscv/rvv/base/vmmv_m_m-2.c: New test.
            * gcc.target/riscv/rvv/base/vmmv_m_m-3.c: New test.
            * gcc.target/riscv/rvv/base/vmnand_mm-1.c: New test.
            * gcc.target/riscv/rvv/base/vmnand_mm-2.c: New test.
            * gcc.target/riscv/rvv/base/vmnand_mm-3.c: New test.
            * gcc.target/riscv/rvv/base/vmnor_mm-1.c: New test.
            * gcc.target/riscv/rvv/base/vmnor_mm-2.c: New test.
            * gcc.target/riscv/rvv/base/vmnor_mm-3.c: New test.
            * gcc.target/riscv/rvv/base/vmnot_m_m-1.c: New test.
            * gcc.target/riscv/rvv/base/vmnot_m_m-2.c: New test.
            * gcc.target/riscv/rvv/base/vmnot_m_m-3.c: New test.
            * gcc.target/riscv/rvv/base/vmor_mm-1.c: New test.
            * gcc.target/riscv/rvv/base/vmor_mm-2.c: New test.
            * gcc.target/riscv/rvv/base/vmor_mm-3.c: New test.
            * gcc.target/riscv/rvv/base/vmorn_mm-1.c: New test.
            * gcc.target/riscv/rvv/base/vmorn_mm-2.c: New test.
            * gcc.target/riscv/rvv/base/vmorn_mm-3.c: New test.
            * gcc.target/riscv/rvv/base/vmsbf_m_m-1.c: New test.
            * gcc.target/riscv/rvv/base/vmsbf_m_m-2.c: New test.
            * gcc.target/riscv/rvv/base/vmsbf_m_m-3.c: New test.
            * gcc.target/riscv/rvv/base/vmsbf_m_mu-1.c: New test.
            * gcc.target/riscv/rvv/base/vmsbf_m_mu-2.c: New test.
            * gcc.target/riscv/rvv/base/vmsbf_m_mu-3.c: New test.
            * gcc.target/riscv/rvv/base/vmset_m_m-1.c: New test.
            * gcc.target/riscv/rvv/base/vmset_m_m-2.c: New test.
            * gcc.target/riscv/rvv/base/vmset_m_m-3.c: New test.
            * gcc.target/riscv/rvv/base/vmsif_m_m-1.c: New test.
            * gcc.target/riscv/rvv/base/vmsif_m_m-2.c: New test.
            * gcc.target/riscv/rvv/base/vmsif_m_m-3.c: New test.
            * gcc.target/riscv/rvv/base/vmsif_m_mu-1.c: New test.
            * gcc.target/riscv/rvv/base/vmsif_m_mu-2.c: New test.
            * gcc.target/riscv/rvv/base/vmsif_m_mu-3.c: New test.
            * gcc.target/riscv/rvv/base/vmsof_m_m-1.c: New test.
            * gcc.target/riscv/rvv/base/vmsof_m_m-2.c: New test.
            * gcc.target/riscv/rvv/base/vmsof_m_m-3.c: New test.
            * gcc.target/riscv/rvv/base/vmsof_m_mu-1.c: New test.
            * gcc.target/riscv/rvv/base/vmsof_m_mu-2.c: New test.
            * gcc.target/riscv/rvv/base/vmsof_m_mu-3.c: New test.
            * gcc.target/riscv/rvv/base/vmxnor_mm-1.c: New test.
            * gcc.target/riscv/rvv/base/vmxnor_mm-2.c: New test.
            * gcc.target/riscv/rvv/base/vmxnor_mm-3.c: New test.
            * gcc.target/riscv/rvv/base/vmxor_mm-1.c: New test.
            * gcc.target/riscv/rvv/base/vmxor_mm-2.c: New test.
            * gcc.target/riscv/rvv/base/vmxor_mm-3.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/base/vmset_m_m-2.c
@@ -0,0 +1,55 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vbool1_t test___riscv_vmset_m_b1(size_t vl)
+{
+    return __riscv_vmset_m_b1(31);
+}
+
+
+vbool2_t test___riscv_vmset_m_b2(size_t vl)
+{
+    return __riscv_vmset_m_b2(31);
+}
+
+
+vbool4_t test___riscv_vmset_m_b4(size_t vl)
+{
+    return __riscv_vmset_m_b4(31);
+}
+
+
+vbool8_t test___riscv_vmset_m_b8(size_t vl)
+{
+    return __riscv_vmset_m_b8(31);
+}
+
+
+vbool16_t test___riscv_vmset_m_b16(size_t vl)
+{
+    return __riscv_vmset_m_b16(31);
+}
+
+
+vbool32_t test___riscv_vmset_m_b32(size_t vl)
+{
+    return __riscv_vmset_m_b32(31);
+}
+
+
+vbool64_t test___riscv_vmset_m_b64(size_t vl)
+{
+    return __riscv_vmset_m_b64(31);
+}
+
+
+
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m8,\s*t[au],\s*m[au]\s+vmset\.m\s+v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m4,\s*t[au],\s*m[au]\s+vmset\.m\s+v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m2,\s*t[au],\s*m[au]\s+vmset\.m\s+v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m1,\s*t[au],\s*m[au]\s+vmset\.m\s+v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf2,\s*t[au],\s*m[au]\s+vmset\.m\s+v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf4,\s*t[au],\s*m[au]\s+vmset\.m\s+v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf8,\s*t[au],\s*m[au]\s+vmset\.m\s+v[0-9]+} 1 } } */
---
+++ gcc/testsuite/gcc.target/riscv/rvv/base/vmxor_mm-2.c
@@ -0,0 +1,55 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv -mabi=lp64d -O3 -fno-schedule-insns -fno-schedule-insns2" } */
+
+#include "riscv_vector.h"
+
+vbool1_t test___riscv_vmxor_mm_b1(vbool1_t op1,vbool1_t op2,size_t vl)
+{
+    return __riscv_vmxor_mm_b1(op1,op2,31);
+}
+
+
+vbool2_t test___riscv_vmxor_mm_b2(vbool2_t op1,vbool2_t op2,size_t vl)
+{
+    return __riscv_vmxor_mm_b2(op1,op2,31);
+}
+
+
+vbool4_t test___riscv_vmxor_mm_b4(vbool4_t op1,vbool4_t op2,size_t vl)
+{
+    return __riscv_vmxor_mm_b4(op1,op2,31);
+}
+
+
+vbool8_t test___riscv_vmxor_mm_b8(vbool8_t op1,vbool8_t op2,size_t vl)
+{
+    return __riscv_vmxor_mm_b8(op1,op2,31);
+}
+
+
+vbool16_t test___riscv_vmxor_mm_b16(vbool16_t op1,vbool16_t op2,size_t vl)
+{
+    return __riscv_vmxor_mm_b16(op1,op2,31);
+}
+
+
+vbool32_t test___riscv_vmxor_mm_b32(vbool32_t op1,vbool32_t op2,size_t vl)
+{
+    return __riscv_vmxor_mm_b32(op1,op2,31);
+}
+
+
+vbool64_t test___riscv_vmxor_mm_b64(vbool64_t op1,vbool64_t op2,size_t vl)
+{
+    return __riscv_vmxor_mm_b64(op1,op2,31);
+}
+
+
+
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m8,\s*t[au],\s*m[au]\s+vmxor\.mm\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m4,\s*t[au],\s*m[au]\s+vmxor\.mm\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m2,\s*t[au],\s*m[au]\s+vmxor\.mm\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*m1,\s*t[au],\s*m[au]\s+vmxor\.mm\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf2,\s*t[au],\s*m[au]\s+vmxor\.mm\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf4,\s*t[au],\s*m[au]\s+vmxor\.mm\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+} 1 } } */
+/* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*31,\s*e8,\s*mf8,\s*t[au],\s*m[au]\s+vmxor\.mm\s+v[0-9]+,\s*v[0-9]+,\s*v[0-9]+} 1 } } */
