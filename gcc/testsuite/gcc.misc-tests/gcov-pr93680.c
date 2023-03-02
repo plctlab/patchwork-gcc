de3fe1982e410c2cc1e1fce8fe80ff1812595675
Xionghu Luo <xionghuluo@tencent.com>
2023-03-02 10:29:20 +0800

    gcov: Fix "do-while" structure in case statement leads to incorrect code coverage [PR93680]
    When spliting edge with self loop, the split edge should be placed just next to
    the edge_in->src, otherwise it may generate different position latch bbs for
    two consecutive self loops.  For details, please refer to:
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=93680#c4
    
    Regression tested pass on x86_64-linux-gnu and aarch64-linux-gnu, OK for
    master?
    
    gcc/ChangeLog:
    
    	PR gcov/93680
    	* tree-cfg.cc (split_edge_bb_loc): Return edge_in->src for self loop.
    
    gcc/testsuite/ChangeLog:
    
    	PR gcov/93680
    	* gcc.misc-tests/gcov-pr93680.c: New test.
    
    Signed-off-by: Xionghu Luo <xionghuluo@tencent.com>

---
+++ gcc/testsuite/gcc.misc-tests/gcov-pr93680.c
@@ -0,0 +1,24 @@
+/* { dg-options "-fprofile-arcs -ftest-coverage" } */
+/* { dg-do run { target native } } */
+
+int f(int s, int n)
+{
+  int p = 0;
+
+  switch (s)
+  {
+    case 0: /* count(5) */
+      do { p++; } while (--n); /* count(5) */
+      return p; /* count(1) */
+
+    case 1: /* count(5) */
+      do { p++; } while (--n); /* count(5) */
+      return p; /* count(1) */
+  }
+
+  return 0;
+}
+
+int main() { f(0, 5); f(1, 5); return 0; }
+
+/* { dg-final { run-gcov gcov-pr93680.c } } */
