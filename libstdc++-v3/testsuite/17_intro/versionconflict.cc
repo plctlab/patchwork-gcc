113125adca4524d76bc087b75981d79c3101bb43
Arsen Arsenović <arsen@aarsen.me>
2023-04-05 01:09:48 +0200

    libstdc++: Add a test for <version> FTM redefinitions
    This test detects redefinitions by compiling stdc++.h with
    -Wsystem-headers.  Thanks Patrick Palka for the suggestion.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/17_intro/versionconflict.cc: New test.

---
+++ libstdc++-v3/testsuite/17_intro/versionconflict.cc
@@ -0,0 +1,6 @@
+// { dg-do preprocess }
+// { dg-additional-options "-Wsystem-headers -Werror" }
+
+// Test for redefinitions of FTMs using bits/stdc++.h.
+#include <bits/stdc++.h>
+#include <version>
