580bb94f3bf10290f811e8ae98bcc4ff50df91d4
Michael Collison <collison@rivosinc.com>
2023-04-17 14:36:57 -0400

    RISC-V:autovec: Add autovectorization tests for add & sub
    2023-03-02  Michael Collison  <collison@rivosinc.com>
    	    Vineet Gupta <vineetg@rivosinc.com>
    
    	* gcc.target/riscv/rvv/autovec: New directory
    	for autovectorization tests.
    	* gcc.target/riscv/rvv/autovec/loop-add-rv32.c: New
    	test to verify code generation of vector add on rv32.
    	* gcc.target/riscv/rvv/autovec/loop-add.c: New
    	test to verify code generation of vector add on rv64.
    	* gcc.target/riscv/rvv/autovec/loop-sub-rv32.c: New
    	test to verify code generation of vector subtract on rv32.
    	* gcc.target/riscv/rvv/autovec/loop-sub.c: New
    	test to verify code generation of vector subtract on rv64.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/loop-sub.c
@@ -0,0 +1,24 @@
+/* { dg-do compile } */
+/* { dg-additional-options "-O2 -ftree-vectorize -march=rv64gcv -mabi=lp64d" } */
+
+#include <stdint.h>
+
+#define TEST_TYPE(TYPE) 				\
+  void vadd_##TYPE (TYPE *dst, TYPE *a, TYPE *b, int n)	\
+  {							\
+    for (int i = 0; i < n; i++)				\
+      dst[i] = a[i] - b[i];				\
+  }
+
+/* *int8_t not autovec currently. */
+#define TEST_ALL()	\
+ TEST_TYPE(int16_t)	\
+ TEST_TYPE(uint16_t)	\
+ TEST_TYPE(int32_t)	\
+ TEST_TYPE(uint32_t)	\
+ TEST_TYPE(int64_t)	\
+ TEST_TYPE(uint64_t)
+
+TEST_ALL()
+
+/* { dg-final { scan-assembler-times {\tvsub\.vv} 6 } } */
