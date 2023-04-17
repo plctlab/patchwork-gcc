107ba56dd61f0b5545e37e368d4144c28377d06b
Michael Collison <collison@rivosinc.com>
2023-04-17 14:36:52 -0400

    RISC-V: Add new predicates and function prototypes
    2023-03-02  Michael Collison  <collison@rivosinc.com>
    	    Juzhe Zhong  <juzhe.zhong@rivai.ai>
    
    	* config/riscv/riscv-protos.h (riscv_classify_vlmul_field):
    	New external declaration.
    	(riscv_vector_preferred_simd_mode): Ditto.
    	(riscv_tuple_mode_p): Ditto.
    	(riscv_vector_mask_mode_p): Ditto.
    	(riscv_classify_nf): Ditto.
    	(riscv_vlmul_regsize): Ditto.
    	(riscv_vector_preferred_simd_mode): Ditto.
    	(riscv_vector_get_mask_mode): Ditto.
    	(emit_vlmax_vsetvl): Ditto.
    	(get_mask_policy_no_pred): Ditto.
    	(get_tail_policy_no_pred): Ditto.
    	* config/riscv/riscv-opts.h (riscv_vector_bits_enum): New enum.
    	(riscv_vector_lmul_enum): Ditto.
    	(vlmul_field_enum): Ditto.
    	* config/riscv/riscv-v.cc (emit_vlmax_vsetvl):
    	Remove static scope.
    	* config/riscv/riscv.opt (riscv_vector_lmul):
    	New option -mriscv_vector_lmul.
    	* config/riscv/predicates.md (p_reg_or_const_csr_operand):
    	New predicate.
    	(vector_reg_or_const_dup_operand): Ditto.

--- gcc/config/riscv/riscv-opts.h
+++ gcc/config/riscv/riscv-opts.h
@@ -67,6 +67,46 @@ enum stack_protector_guard {
   SSP_GLOBAL			/* global canary */
 };
 
+/* RVV vector register sizes.  */
+enum riscv_vector_bits_enum
+{
+  RVV_SCALABLE,
+  RVV_NOT_IMPLEMENTED = RVV_SCALABLE,
+  RVV_64 = 64,
+  RVV_128 = 128,
+  RVV_256 = 256,
+  RVV_512 = 512,
+  RVV_1024 = 1024,
+  RVV_2048 = 2048,
+  RVV_4096 = 4096,
+  RVV_8192 = 8192,
+  RVV_16384 = 16384,
+  RVV_32768 = 32768,
+  RVV_65536 = 65536
+};
+
+/* vectorization factor.  */
+enum riscv_vector_lmul_enum
+{
+  RVV_LMUL1 = 1,
+  RVV_LMUL2 = 2,
+  RVV_LMUL4 = 4,
+  RVV_LMUL8 = 8
+};
+
+enum vlmul_field_enum
+{
+  VLMUL_FIELD_000, /* LMUL = 1.  */
+  VLMUL_FIELD_001, /* LMUL = 2.  */
+  VLMUL_FIELD_010, /* LMUL = 4.  */
+  VLMUL_FIELD_011, /* LMUL = 8.  */
+  VLMUL_FIELD_100, /* RESERVED.  */
+  VLMUL_FIELD_101, /* LMUL = 1/8.  */
+  VLMUL_FIELD_110, /* LMUL = 1/4.  */
+  VLMUL_FIELD_111, /* LMUL = 1/2.  */
+  MAX_VLMUL_FIELD
+};
+
 #define MASK_ZICSR    (1 << 0)
 #define MASK_ZIFENCEI (1 << 1)
 
