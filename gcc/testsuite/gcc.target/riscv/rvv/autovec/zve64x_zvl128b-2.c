0ba1ed6f7b9aecaa2baf980a3086e1a0e34470fa
Ju-Zhe Zhong <juzhe.zhong@rivai.ai>
2023-04-20 00:42:14 +0800

    RISC-V: Add sanity testcases for RVV auto-vectorization
    This patch adds sanity tests for basic enabling auto-vectorization.
    We should make sure compiler enable auto-vectorization strictly according
    to '-march'
    
    For example, '-march=rv32gc_zve32x' can not allow INT64 auto-vectorization.
    Since SEW = 64 RVV instructions are illegal instructions in this situation.
    
    Also, testing auto-vectoriztion for all combinations of LMUL = 1/2/4/8
    gcc/testsuite/ChangeLog:
    
            * gcc.target/riscv/rvv/rvv.exp: Add auto-vectorization tests.
            * gcc.target/riscv/rvv/autovec/partial/single_rgroup-1.c: New test.
            * gcc.target/riscv/rvv/autovec/partial/single_rgroup-1.h: New test.
            * gcc.target/riscv/rvv/autovec/partial/single_rgroup_run-1.c: New test.
            * gcc.target/riscv/rvv/autovec/template-1.h: New test.
            * gcc.target/riscv/rvv/autovec/v-1.c: New test.
            * gcc.target/riscv/rvv/autovec/v-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32f-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32f-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32f-3.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32f_zvl128b-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32f_zvl128b-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32x-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32x-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32x-3.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32x_zvl128b-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve32x_zvl128b-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64d-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64d-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64d-3.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64d_zvl128b-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64d_zvl128b-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64f-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64f-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64f-3.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64f_zvl128b-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64f_zvl128b-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64x-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64x-2.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64x-3.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64x_zvl128b-1.c: New test.
            * gcc.target/riscv/rvv/autovec/zve64x_zvl128b-2.c: New test.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/zve64x_zvl128b-2.c
@@ -0,0 +1,6 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32gc_zve64x_zvl128b -mabi=ilp32d --param riscv-autovec-preference=fixed-vlmax -O3 -ftree-vectorize -fdump-tree-vect-details -save-temps" } */
+
+#include "template-1.h"
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops in function" 3 "vect" } } */
