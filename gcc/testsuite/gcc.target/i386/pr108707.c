a19a3d07588ed089f3bed4c614a34fef0b4a0e0e
liuhongt <hongtao.liu@intel.com>
2023-04-20 08:46:15 +0800

    Adjust testcases after better RA decision.
    After optimization for RA, memory op is not propagated into
    instructions(>1), and it make testcases not generate vxorps since
    the memory is loaded into the dest, and the dest is never unused now.
    
    So rewrite testcases to make the codegen more stable.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/i386/avx2-dest-false-dep-for-glc.c: Rewrite
    	testcase to make the codegen more stable.
    	* gcc.target/i386/avx512dq-dest-false-dep-for-glc.c: Ditto
    	* gcc.target/i386/avx512f-dest-false-dep-for-glc.c: Ditto.
    	* gcc.target/i386/avx512fp16-dest-false-dep-for-glc.c: Ditto.
    	* gcc.target/i386/avx512vl-dest-false-dep-for-glc.c: Ditto.

--- gcc/testsuite/gcc.target/i386/pr108707.c
+++ gcc/testsuite/gcc.target/i386/pr108707.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-mavx512f -O2" } */
-/* { dg-final { scan-assembler-not {(?n)vfmadd[1-3]*ps.*\(} } } */
+/* { dg-final { scan-assembler-not {(?n)vfmadd[1-3]*ps.*\(} { target { ! ia32 } } } } */
 /* { dg-final { scan-assembler-times {(?n)vfmadd[1-3]*ps[ \t]*} 3 } } */
 
 #include<immintrin.h>
