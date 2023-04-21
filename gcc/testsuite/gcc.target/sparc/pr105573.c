b562054703ed1174d4288eff05bc228019b09f7c
Sam James <sam@gentoo.org>
2023-04-21 20:33:38 +0100

    testsuite: Add testcase for sparc ICE [PR105573]
    r11-10018-g33914983cf3734c2f8079963ba49fcc117499ef3 fixed PR105312 and added
    a test case for target/arm but the duplicate PR105573 has a test case for
    target/sparc that was uncommitted until now.
    
    2023-04-21  Sam James   <sam@gentoo.org>
            PR tree-optimization/105312
    	PR target/105573
    	* gcc/testsuite/gcc.target/sparc/pr105573.c: New test.
    
    Signed-off-by: Sam James <sam@gentoo.org>

---
+++ gcc/testsuite/gcc.target/sparc/pr105573.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mvis3" } */
+
+int *UINT_sign_args, UINT_sign_steps;
+int *UINT_sign_ip1;
+__attribute__((optimize("O3"))) void UINT_sign() {
+  char *op1 = (char*) UINT_sign_args;
+  int os1 = UINT_sign_steps, i;
+  for (; i; i++, op1 += os1) {
+    unsigned in = *(unsigned *)UINT_sign_ip1;
+    int *out = (int*) op1;
+    *out = in > 0;
+  }
+}
