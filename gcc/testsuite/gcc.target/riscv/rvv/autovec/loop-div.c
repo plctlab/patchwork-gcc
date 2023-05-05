5b19c4b083d6aba7420a271898ddb6c576b9d490
Michael Collison <collison@rivosinc.com>
2023-05-05 11:46:06 -0400

    RISC-V:autovec: Add autovectorization tests for binary integer
    2023-04-05  Michael Collison  <collison@rivosinc.com>
    
    	* gcc.target/riscv/rvv/autovec/loop-and-rv32.c: New
            test to verify code generation of vector "and" on rv32.
            * gcc.target/riscv/rvv/autovec/loop-and.c: New
            test to verify code generation of vector "and" on rv64.
            * gcc.target/riscv/rvv/autovec/loop-div-rv32.c: New
            test to verify code generation of vector divide on rv32.
            * gcc.target/riscv/rvv/autovec/loop-div.c: New
            test to verify code generation of vector divide on rv64.
            * gcc.target/riscv/rvv/autovec/loop-max-rv32.c: New
            test to verify code generation of vector maximum on rv32.
            * gcc.target/riscv/rvv/autovec/loop-max.c: New
            test to verify code generation of vector maximum on rv64.
            * gcc.target/riscv/rvv/autovec/loop-min-rv32.c: New
            test to verify code generation of vector minimum on rv32.
            * gcc.target/riscv/rvv/autovec/loop-min.c: New
            test to verify code generation of vector minimum on rv64.
            * gcc.target/riscv/rvv/autovec/loop-mod-rv32.c: New
            test to verify code generation of vector modulus on rv32.
            * gcc.target/riscv/rvv/autovec/loop-mod.c: New
            test to verify code generation of vector modulus on rv64.
            * gcc.target/riscv/rvv/autovec/loop-mul-rv32.c: New
            test to verify code generation of vector multiply on rv32.
            * gcc.target/riscv/rvv/autovec/loop-mul.c: New
            test to verify code generation of vector multiply on rv64.
            * gcc.target/riscv/rvv/autovec/loop-or-rv32.c: New
            test to verify code generation of vector "or" on rv32.
            * gcc.target/riscv/rvv/autovec/loop-or.c: New
            test to verify code generation of vector "or" on rv64.
            * gcc.target/riscv/rvv/autovec/loop-xor-rv32.c: New
            test to verify code generation of vector xor on rv32.
            * gcc.target/riscv/rvv/autovec/loop-xor.c: New
            test to verify code generation of vector xor on rv64.

---
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/loop-div.c
@@ -0,0 +1,25 @@
+/* { dg-do compile } */
+/* { dg-additional-options "-O2 -ftree-vectorize -march=rv64gcv -mabi=lp64d" } */
+
+#include <stdint.h>
+
+#define TEST_TYPE(TYPE) 				\
+  void vdiv_##TYPE (TYPE *dst, TYPE *a, TYPE *b, int n)	\
+  {							\
+    for (int i = 0; i < n; i++)				\
+      dst[i] = a[i] / b[i];				\
+  }
+
+/* *int8_t not autovec currently. */
+#define TEST_ALL()	\
+ TEST_TYPE(int16_t)	\
+ TEST_TYPE(uint16_t)	\
+ TEST_TYPE(int32_t)	\
+ TEST_TYPE(uint32_t)	\
+ TEST_TYPE(int64_t)	\
+ TEST_TYPE(uint64_t)
+
+TEST_ALL()
+
+/* { dg-final { scan-assembler-times {\tvdiv\.vv} 3 } } */
+/* { dg-final { scan-assembler-times {\tvdivu\.vv} 3 } } */
