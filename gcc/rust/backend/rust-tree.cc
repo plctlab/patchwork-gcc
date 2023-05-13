275899799e8e9138b69eaca53d82b07ea6462e07
Bernhard Reutner-Fischer <aldot@gcc.gnu.org>
2023-05-14 01:23:16 +0200

    rust: use _P() defines from tree.h
    gcc/rust/ChangeLog:
    
    	* backend/rust-compile-expr.cc (CompileExpr::type_cast_expression): Use
    	_P() defines from tree.h
    	* backend/rust-tree.cc (build_cplus_array_type): Ditto.
    	* backend/rust-tree.h (ARITHMETIC_TYPE_P): Ditto.
    	(gnu_vector_type_p): Ditto.
    	* checks/lints/rust-lint-unused-var.cc (check_decl): Ditto.
    	* rust-gcc.cc (Gcc_backend::fill_in_array): Ditto.
    	(Gcc_backend::named_type): Ditto.
    	(Gcc_backend::convert_expression): Ditto.
    	(Gcc_backend::init_statement): Ditto.

--- gcc/rust/backend/rust-tree.cc
+++ gcc/rust/backend/rust-tree.cc
@@ -2404,7 +2404,7 @@ build_cplus_array_type (tree elt_type, tree index_type, int dependent)
     }
 
   /* Avoid spurious warnings with VLAs (c++/54583).  */
-  if (TYPE_SIZE (t) && EXPR_P (TYPE_SIZE (t)))
+  if (CAN_HAVE_LOCATION_P (TYPE_SIZE (t)))
     suppress_warning (TYPE_SIZE (t), OPT_Wunused);
 
   /* Push these needs up to the ARRAY_TYPE so that initialization takes
