d823b71ebbc3a2f196a0ef2dd5be875afd8be482
Bob Duff <duff@adacore.com>
2023-05-15 11:44:22 +0200

    ada: Fix comment related to inlining
    Correction to previous check-in: Remove comment about
    Proc_Next_... procedures, which were deleted.
    
    gcc/ada/
    
    	* einfo-utils.ads: Remove comment.
    
    Tested on x86_64-pc-linux-gnu, committed on master.

--- gcc/ada/einfo-utils.ads
+++ gcc/ada/einfo-utils.ads
@@ -451,9 +451,7 @@ package Einfo.Utils is
    -- Iterators --
    ---------------
 
-   --  The call to Next_xxx (obj) is equivalent to obj := Next_xxx (obj)
-   --  We define the set of Proc_Next_xxx routines simply for the purposes
-   --  of inlining them without necessarily inlining the function.
+   --  Next_xxx (obj) is equivalent to obj := Next_xxx (obj)
 
    procedure Proc_Next_Component                 (N : in out Node_Id);
    procedure Proc_Next_Component_Or_Discriminant (N : in out Node_Id);
