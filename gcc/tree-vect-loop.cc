fbe50fb71def3feff5f7396ab1fd78ecf8e552a2
Juzhe-Zhong <juzhe.zhong@rivai.ai>
2023-05-15 09:28:44 +0800

    VECT: Add decrement IV support in Loop Vectorizer
    This patch implement decrement IV for length approach in loop control.
    
    Address comment from kewen that incorporate the implementation inside
    "vect_set_loop_controls_directly" instead of a standalone function.
    
    Address comment from Richard using MIN_EXPR to handle these 3 following
    cases
    1. single rgroup.
    2. multiple rgroup for SLP.
    3. multiple rgroup for non-SLP (tested on vec_pack_trunc).
    
    Bootstraped && Regression on x86.
    
    Ok for trunk ?
    
    gcc/ChangeLog:
    
            * tree-vect-loop-manip.cc (vect_adjust_loop_lens): New function.
            (vect_set_loop_controls_directly): Add decrement IV support.
            (vect_set_loop_condition_partial_vectors): Ditto.
            * tree-vect-loop.cc (_loop_vec_info::_loop_vec_info): Add a new variable.
            (vect_get_loop_len): Add decrement IV support.
            * tree-vect-stmts.cc (vectorizable_store): Ditto.
            (vectorizable_load): Ditto.
            * tree-vectorizer.h (LOOP_VINFO_USING_DECREMENTING_IV_P): New macro.
            (vect_get_loop_len): Add decrement IV support.

--- gcc/tree-vect-loop.cc
+++ gcc/tree-vect-loop.cc
@@ -973,6 +973,7 @@ _loop_vec_info::_loop_vec_info (class loop *loop_in, vec_info_shared *shared)
     vectorizable (false),
     can_use_partial_vectors_p (param_vect_partial_vector_usage != 0),
     using_partial_vectors_p (false),
+    using_decrementing_iv_p (false),
     epil_using_partial_vectors_p (false),
     partial_load_store_bias (0),
     peeling_for_gaps (false),
@@ -2724,6 +2725,17 @@ start_over:
       && !vect_verify_full_masking (loop_vinfo)
       && !vect_verify_loop_lens (loop_vinfo))
     LOOP_VINFO_CAN_USE_PARTIAL_VECTORS_P (loop_vinfo) = false;
+
+  /* If we're vectorizing an loop that uses length "controls" and
+     can iterate more than once, we apply decrementing IV approach
+     in loop control.  */
+  if (LOOP_VINFO_CAN_USE_PARTIAL_VECTORS_P (loop_vinfo)
+      && !LOOP_VINFO_LENS (loop_vinfo).is_empty ()
+      && !(LOOP_VINFO_NITERS_KNOWN_P (loop_vinfo)
+	   && LOOP_VINFO_VECT_FACTOR (loop_vinfo).is_constant ()
+	   && LOOP_VINFO_INT_NITERS (loop_vinfo)
+		<= LOOP_VINFO_VECT_FACTOR (loop_vinfo).is_constant ()))
+    LOOP_VINFO_USING_DECREMENTING_IV_P (loop_vinfo) = true;
 
   /* If we're vectorizing an epilogue loop, the vectorized loop either needs
      to be able to handle fewer than VF scalars, or needs to have a lower VF
@@ -10364,12 +10376,14 @@ vect_record_loop_len (loop_vec_info loop_vinfo, vec_loop_lens *lens,
    rgroup that operates on NVECTORS vectors, where 0 <= INDEX < NVECTORS.  */
 
 tree
-vect_get_loop_len (loop_vec_info loop_vinfo, vec_loop_lens *lens,
-		   unsigned int nvectors, unsigned int index)
+vect_get_loop_len (loop_vec_info loop_vinfo, gimple_stmt_iterator *gsi,
+		   vec_loop_lens *lens, unsigned int nvectors, tree vectype,
+		   unsigned int index)
 {
   rgroup_controls *rgl = &(*lens)[nvectors - 1];
   bool use_bias_adjusted_len =
     LOOP_VINFO_PARTIAL_LOAD_STORE_BIAS (loop_vinfo) != 0;
+  tree iv_type = LOOP_VINFO_RGROUP_IV_TYPE (loop_vinfo);
 
   /* Populate the rgroup's len array, if this is the first time we've
      used it.  */
@@ -10400,6 +10414,26 @@ vect_get_loop_len (loop_vec_info loop_vinfo, vec_loop_lens *lens,
 
   if (use_bias_adjusted_len)
     return rgl->bias_adjusted_ctrl;
+  else if (LOOP_VINFO_USING_DECREMENTING_IV_P (loop_vinfo))
+    {
+      tree loop_len = rgl->controls[index];
+      poly_int64 nunits1 = TYPE_VECTOR_SUBPARTS (rgl->type);
+      poly_int64 nunits2 = TYPE_VECTOR_SUBPARTS (vectype);
+      if (maybe_ne (nunits1, nunits2))
+	{
+	  /* A loop len for data type X can be reused for data type Y
+	     if X has N times more elements than Y and if Y's elements
+	     are N times bigger than X's.  */
+	  gcc_assert (multiple_p (nunits1, nunits2));
+	  unsigned int factor = exact_div (nunits1, nunits2).to_constant ();
+	  gimple_seq seq = NULL;
+	  loop_len = gimple_build (&seq, RDIV_EXPR, iv_type, loop_len,
+				   build_int_cst (iv_type, factor));
+	  if (seq)
+	    gsi_insert_seq_before (gsi, seq, GSI_SAME_STMT);
+	}
+      return loop_len;
+    }
   else
     return rgl->controls[index];
 }
