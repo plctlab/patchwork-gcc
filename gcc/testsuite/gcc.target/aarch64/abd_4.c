123661280b2521743c8c43efecfdfc21b6ccd892
oluade01 <oluwatamilore.adebayo@arm.com>
2023-06-06 16:17:06 +0100

    AArch64: New RTL for ABD
    This patch adds new RTL and tests for sabd and uabd
    
    PR tree-optimization/109156
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-simd-builtins.def (sabd, uabd):
    	Change the mode to 3.
    	* config/aarch64/aarch64-simd.md (aarch64_<su>abd<mode>):
    	Rename to <su>abd<mode>3.
    	* config/aarch64/aarch64-sve.md (<su>abd<mode>_3): Rename
    	to <su>abd<mode>3.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/abd.h: New file.
    	* gcc.target/aarch64/abd_2.c: New test.
    	* gcc.target/aarch64/abd_3.c: New test.
    	* gcc.target/aarch64/abd_4.c: New test.
    	* gcc.target/aarch64/abd_none_2.c: New test.
    	* gcc.target/aarch64/abd_none_3.c: New test.
    	* gcc.target/aarch64/abd_none_4.c: New test.
    	* gcc.target/aarch64/abd_run_1.c: New test.
    	* gcc.target/aarch64/sve/abd_1.c: New test.
    	* gcc.target/aarch64/sve/abd_none_1.c: New test.
    	* gcc.target/aarch64/sve/abd_2.c: New test.
    	* gcc.target/aarch64/sve/abd_none_2.c: New test.

---
+++ gcc/testsuite/gcc.target/aarch64/abd_4.c
@@ -0,0 +1,36 @@
+/* { dg-do compile } */
+/* { dg-options "-O3" } */
+
+#pragma GCC target "+nosve"
+#define N 1024
+
+#define ABD_IDIOM
+#include "abd.h"
+
+TEST1(signed, int)
+TEST1(signed, short)
+TEST1(signed, char)
+
+TEST2(signed, char, short)
+TEST2(signed, char, int)
+TEST2(signed, short, int)
+
+TEST3(signed, char, int, short)
+TEST3(signed, char, short, int)
+
+TEST1(unsigned, int)
+TEST1(unsigned, short)
+TEST1(unsigned, char)
+
+TEST2(unsigned, char, short)
+TEST2(unsigned, char, int)
+TEST2(unsigned, short, int)
+
+TEST3(unsigned, char, short, int)
+TEST3(unsigned, char, int, short)
+
+/* { dg-final { scan-assembler-times "sabd\\tv\[0-9\]+\.4s, v\[0-9\]+\.4s, v\[0-9\]+\.4s" 2 } } */
+/* { dg-final { scan-assembler-times "sabd\\tv\[0-9\]+\.8h, v\[0-9\]+\.8h, v\[0-9\]+\.8h" 9 } } */
+/* { dg-final { scan-assembler-times "sabd\\tv\[0-9\]+\.16b, v\[0-9\]+\.16b, v\[0-9\]+\.16b" 4 } } */
+/* { dg-final { scan-assembler-times "uabd\\tv\[0-9\]+\.8h, v\[0-9\]+\.8h, v\[0-9\]+\.8h" 3 } } */
+/* { dg-final { scan-assembler-times "uabd\\tv\[0-9\]+\.16b, v\[0-9\]+\.16b, v\[0-9\]+\.16b" 2 } } */
