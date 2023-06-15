b6f7e71e799a2ce0308ef04c424bcc6d7d5f4701
Piotr Trojanek <trojanek@adacore.com>
2023-06-15 10:04:10 +0200

    ada: Accept aspect Always_Terminates on packages
    The recently added aspect Always_Terminates is now allowed on packages
    and generic packages, but only when it has no arguments. The intuitive
    meaning is that all subprograms declared in such a package are always
    terminating.
    
    gcc/ada/
    
    	* contracts.adb (Add_Contract_Item): Add pragma Always_Terminates to
    	package contract.
    	* sem_prag.adb (Analyze_Pragma): Accept pragma Always_Terminates on
    	packages and generic packages, but only when it has no arguments.
    
    Tested on x86_64-pc-linux-gnu, committed on master.

--- gcc/ada/sem_prag.adb
+++ gcc/ada/sem_prag.adb
@@ -11036,6 +11036,17 @@ package body Sem_Prag is
                return;
             end if;
 
+            --  Pragma Always_Terminates applied to packages doesn't allow any
+            --  expression.
+
+            if Is_Package_Or_Generic_Package (Spec_Id)
+              and then Arg_Count /= 0
+            then
+               Error_Msg_N (Fix_Error
+                 ("pragma % applied to package cannot have arguments"), N);
+               return;
+            end if;
+
             --  A pragma that applies to a Ghost entity becomes Ghost for the
             --  purposes of legality checks and removal of ignored Ghost code.
 
@@ -24317,9 +24328,14 @@ package body Sem_Prag is
             Subp_Decl :=
               Find_Related_Declaration_Or_Body (N, Do_Checks => True);
 
-            --  Generic subprogram
+            --  Generic subprogram and package declaration
 
-            if Nkind (Subp_Decl) = N_Generic_Subprogram_Declaration then
+            if Nkind (Subp_Decl) in N_Generic_Declaration then
+               null;
+
+            --  Package declaration
+
+            elsif Nkind (Subp_Decl) = N_Package_Declaration then
                null;
 
             --  Body acts as spec
