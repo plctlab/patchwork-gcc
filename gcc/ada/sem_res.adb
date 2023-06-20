ca0698e574e4bdbb8e12afcde482d21902ba9d6a
Yannick Moy <moy@adacore.com>
2023-06-20 13:26:33 +0200

    ada: Fix crash on inlining in GNATprove
    After the recent change on detection of non-inlining, calls inside
    the iterator part of a quantified expression were not considered
    as preventing inlining anymore, leading to a crash later on inside
    GNATprove. Now fixed.
    
    gcc/ada/
    
    	* sem_res.adb (Resolve_Call): Fix change that replaced test for
    	quantified expressions by the test for potentially unevaluated
    	contexts. Both should be performed.
    
    Tested on x86_64-pc-linux-gnu, committed on master.

--- gcc/ada/sem_res.adb
+++ gcc/ada/sem_res.adb
@@ -7289,6 +7289,15 @@ package body Sem_Res is
                Cannot_Inline
                  ("cannot inline & (in quantified expression)?", N, Nam_UA);
 
+            --  Calls are not inlined inside the loop_parameter_specification
+            --  or iterator_specification of the quantified expression, as they
+            --  are only preanalyzed. Calls in the predicate part are handled
+            --  by the previous test on potentially unevaluated expressions.
+
+            elsif In_Quantified_Expression (N) then
+               Cannot_Inline
+                 ("cannot inline & (in quantified expression)?", N, Nam_UA);
+
             --  Inlining should not be performed during preanalysis
 
             elsif Full_Analysis then
