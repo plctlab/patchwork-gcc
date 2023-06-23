847fe93cbeead45da5a4d19fc95a03f7263b5d1c
Richard Biener <rguenther@suse.de>
2023-06-23 14:16:07 +0200

    narrowing initializers and initializer_constant_valid_p_1
    initializer_constant_valid_p_1 attempts to handle narrowing
    differences and sums but fails to handle when the overall
    value looks like
    
      VIEW_CONVERT_EXPR<long long int>(NON_LVALUE_EXPR <v>
        -  VEC_COND_EXPR < { 0, 0 } == { 0, 0 } , { -1, -1 } , { 0, 0 } > )
    
    where endtype is scalar integer but value is a vector type.
    In this particular case all is good and we recurse since
    two vector lanes is more than 64bits of long long.  But still
    it compares apples and oranges.
    
    Fixed by appropriately also requiring the type of the
    value to be scalar integral.
    
    Bootstrap and regtest pending on x86_64-unknown-linux-gnu.  This
    seems to be the last fallout in the testsuite for the
    TYPE_PRECISION checking patch.
    
    	* varasm.cc (initializer_constant_valid_p_1): Also
    	constrain the type of value to be scalar integral
    	before dispatching to narrowing_initializer_constant_valid_p.

--- gcc/varasm.cc
+++ gcc/varasm.cc
@@ -4943,6 +4943,7 @@ initializer_constant_valid_p_1 (tree value, tree endtype, tree *cache)
       if (cache && cache[0] == value)
 	return cache[1];
       if (! INTEGRAL_TYPE_P (endtype)
+	  || ! INTEGRAL_TYPE_P (TREE_TYPE (value))
 	  || TYPE_PRECISION (endtype) >= TYPE_PRECISION (TREE_TYPE (value)))
 	{
 	  tree ncache[4] = { NULL_TREE, NULL_TREE, NULL_TREE, NULL_TREE };
@@ -4979,6 +4980,7 @@ initializer_constant_valid_p_1 (tree value, tree endtype, tree *cache)
       if (cache && cache[0] == value)
 	return cache[1];
       if (! INTEGRAL_TYPE_P (endtype)
+	  || ! INTEGRAL_TYPE_P (TREE_TYPE (value))
 	  || TYPE_PRECISION (endtype) >= TYPE_PRECISION (TREE_TYPE (value)))
 	{
 	  tree ncache[4] = { NULL_TREE, NULL_TREE, NULL_TREE, NULL_TREE };
