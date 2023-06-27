932ab578a6976ba3177309fba903e508981c8257
Viljar Indus <indus@adacore.com>
2023-06-27 14:07:45 +0200

    ada: Fix expanding container aggregates
    Ensure that that container aggregate expressions are expanded as
    such and not as records even if the type of the expression is a
    record.
    
    gcc/ada/
    
    	* exp_aggr.adb (Expand_N_Aggregate): Ensure that container
    	aggregate expressions do not get expanded as records but instead
    	as container aggregates.
    
    Tested on x86_64-pc-linux-gnu, committed on master.

--- gcc/ada/exp_aggr.adb
+++ gcc/ada/exp_aggr.adb
@@ -7231,6 +7231,7 @@ package body Exp_Aggr is
 
       if Is_Record_Type (T)
         and then not Is_Private_Type (T)
+        and then not Is_Homogeneous_Aggregate (N)
       then
          Expand_Record_Aggregate (N);
 
