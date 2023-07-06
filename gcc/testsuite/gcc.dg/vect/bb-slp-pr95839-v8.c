e61433b9a5ccf7a572e0e3971b765301d1cf2050
Maciej W. Rozycki <macro@embecosm.com>
2023-07-06 22:36:12 +0100

    testsuite: Require 128-bit vectors for bb-slp-pr95839.c
    The bb-slp-pr95839.c test assumes quad-single float vector support, but
    some targets only support pairs of floats, causing this test to fail
    with such targets.  Limit this test to targets that support at least
    128-bit vectors then, and add a complementing test that can be run with
    targets that have support for 64-bit vectors only.  There is no need to
    adjust bb-slp-pr95839-2.c as 128 bits are needed even for the smallest
    vector of doubles, so support is implied by the presence of vectors of
    doubles.
    
    	gcc/testsuite/
    	* gcc.dg/vect/bb-slp-pr95839.c: Limit to `vect128' targets.
    	* gcc.dg/vect/bb-slp-pr95839-v8.c: New test.

--- gcc/testsuite/gcc.dg/vect/bb-slp-pr95839-v8.c
+++ gcc/testsuite/gcc.dg/vect/bb-slp-pr95839-v8.c
@@ -1,14 +1,14 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target vect_float } */
-/* { dg-require-effective-target vect128 } */
+/* { dg-require-effective-target vect64 } */
 /* { dg-additional-options "-w -Wno-psabi" } */
 
-typedef float __attribute__((vector_size(16))) v4f32;
+typedef float __attribute__((vector_size(8))) v2f32;
 
-v4f32 f(v4f32 a, v4f32 b)
+v2f32 f(v2f32 a, v2f32 b)
 {
   /* Check that we vectorize this CTOR without any loads.  */
-  return (v4f32){a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]};
+  return (v2f32){a[0] + b[0], a[1] + b[1]};
 }
 
 /* { dg-final { scan-tree-dump "optimized: basic block" "slp2" } } */
