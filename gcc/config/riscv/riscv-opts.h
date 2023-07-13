fc78dc1b2c6edd3b0f30f2aa296ee0874d48592a
Monk Chiang <monk.chiang@sifive.com>
2023-07-13 13:38:55 +0800

    RISC-V: Recognized zihintntl extensions
    gcc/ChangeLog:
    
                * common/config/riscv/riscv-common.cc:
                (riscv_implied_info): Add zihintntl item.
                (riscv_ext_version_table): Ditto.
                (riscv_ext_flag_table): Ditto.
                * config/riscv/riscv-opts.h (MASK_ZIHINTNTL): New macro.
                (TARGET_ZIHINTNTL): Ditto.
    
        gcc/testsuite/ChangeLog:
    
                * gcc.target/riscv/arch-22.c: New test.
                * gcc.target/riscv/predef-28.c: New test.

--- gcc/config/riscv/riscv-opts.h
+++ gcc/config/riscv/riscv-opts.h
@@ -93,9 +93,11 @@ enum riscv_multilib_select_kind {
 
 #define MASK_ZICSR    (1 << 0)
 #define MASK_ZIFENCEI (1 << 1)
+#define MASK_ZIHINTNTL (1 << 2)
 
 #define TARGET_ZICSR    ((riscv_zi_subext & MASK_ZICSR) != 0)
 #define TARGET_ZIFENCEI ((riscv_zi_subext & MASK_ZIFENCEI) != 0)
+#define TARGET_ZIHINTNTL ((riscv_zi_subext & MASK_ZIHINTNTL) != 0)
 
 #define MASK_ZAWRS   (1 << 0)
 #define TARGET_ZAWRS ((riscv_za_subext & MASK_ZAWRS) != 0)
