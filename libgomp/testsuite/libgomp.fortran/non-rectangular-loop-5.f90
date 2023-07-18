3c38510ef707c11bc1c1ca4a95f56b6d0387fb61
Tobias Burnus <tobias@codesourcery.com>
2023-07-18 14:11:13 +0200

    OpenMP/Fortran: Non-rectangular loops with constant steps other than 1 or -1 [PR107424]
    Comments regarding the validity of the Fortran assumptions are welcome!
    
    This patch now uses a 'simple' loop for OpenMP loops with
    a constant loop-step size. Before, it only did so for step = ±1.
    (Otherwise, a count variable is used from which the original
    loop index variable is calculated from.)
    
    For details, see the attached patch or
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=107424#c12
    (comment 12 + 14 plus the email linked in comment 12).
    
    Comments? Remarks? If there are none, I will relatively soonish
    commit the attached patch to mainline, only.
    
    Tobias
    -----------------
    Siemens Electronic Design Automation GmbH; Anschrift: Arnulfstraße 201, 80634 München; Gesellschaft mit beschränkter Haftung; Geschäftsführer: Thomas Heurung, Frank Thürauf; Sitz der Gesellschaft: München; Registergericht München, HRB 106955
    
    OpenMP/Fortran: Non-rectangular loops with constant steps other than 1 or -1 [PR107424]
    
    Before this commit, gfortran produced with OpenMP for 'do i = 1,10,2'
    the code
      for (count.0 = 0; count.0 < 5; count.0 = count.0 + 1)
        i = count.0 * 2 + 1;
    
    While such an inner loop can be collapsed, a non-rectangular could not.
    With this commit and for all constant loop steps, a simple loop such
    as 'for (i = 1; i <= 10; i = i + 2)' is created. (Before only for the
    constant steps of 1 and -1.)
    
    The constant step permits to know the direction (increasing/decreasing)
    that is required for the loop condition.
    
    The new code is only valid if one assumes no overflow of the loop variable.
    However, the Fortran standard can be read that this must be ensured by
    the user. Namely, the Fortran standard requires (F2023, 10.1.5.2.4):
    "The execution of any numeric operation whose result is not defined by
    the arithmetic used by the processor is prohibited."
    
    And, for DO loops, F2023's "11.1.7.4.3 The execution cycle" has the
    following: The number of loop iterations handled by an iteration count,
    which would permit code like 'do i = huge(i)-5, huge(i),4'. However,
    in step (3), this count is not only decremented by one but also:
      "... The DO variable, if any, is incremented by the value of the
      incrementation parameter m3."
    And for the example above, 'i' would be 'huge(i)+3' in the last
    execution cycle, which exceeds the largest model number and should
    render the example as invalid.
    
    	PR fortran/107424
    
    gcc/fortran/ChangeLog:
    
    	* trans-openmp.cc (gfc_nonrect_loop_expr): Accept all
    	constant loop steps.
    	(gfc_trans_omp_do): Likewise; use sign to determine
    	loop direction.
    
    libgomp/ChangeLog:
    
    	* testsuite/libgomp.fortran/non-rectangular-loop-1.f90: Enabled
    	commented tests.
    	* testsuite/libgomp.fortran/non-rectangular-loop-1a.f90: Removed.
    	* testsuite/libgomp.fortran/non-rectangular-loop-5.f90: Change
    	testcase to use a non-constant step to retain the 'sorry' test.
            * testsuite/libgomp.fortran/non-rectangular-loop-6.f90: New test.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/gomp/linear-2.f90: Update dump to remove
    	the additional count variable.
    
     gcc/fortran/trans-openmp.cc                        |  12 +-
     gcc/testsuite/gfortran.dg/gomp/linear-2.f90        |   4 +-
     .../libgomp.fortran/non-rectangular-loop-1.f90     | 537 ++++++++++-----------
     .../libgomp.fortran/non-rectangular-loop-1a.f90    | 374 --------------
     .../libgomp.fortran/non-rectangular-loop-5.f90     |  10 +-
     .../libgomp.fortran/non-rectangular-loop-6.f90     | 196 ++++++++
     6 files changed, 477 insertions(+), 656 deletions(-)

--- libgomp/testsuite/libgomp.fortran/non-rectangular-loop-5.f90
+++ libgomp/testsuite/libgomp.fortran/non-rectangular-loop-5.f90
@@ -6,22 +6,26 @@
 
 ! Nonrectangular loop nests checks
 
+integer :: step
+step = -1
 !$omp simd collapse(2)
 do i = 1, 10
-  do j = i, 10, 2  ! { dg-message "sorry, unimplemented: non-rectangular loop nest with step other than constant 1 or -1 for 'j'" }
+  do j = i, 10, step  ! { dg-message "sorry, unimplemented: non-rectangular loop nest with non-constant step for 'j'" }
   end do
 end do
 
+step = 3
 !$omp do collapse(2) lastprivate(j)  ! { dg-error "lastprivate variable 'j' is private in outer context" }
 do i = 1, 10
-  do j = i, 10, 2  ! { dg-message "sorry, unimplemented: non-rectangular loop nest with step other than constant 1 or -1 for 'j'" }
+  do j = i, 10, step  ! { dg-message "sorry, unimplemented: non-rectangular loop nest with non-constant step for 'j'" }
   end do
 end do
 if (i /= 11) stop 1
 
+step = -5
 !$omp simd collapse(2) lastprivate(j)
 do i = 1, 10
-  do j = i, 10, 2  ! { dg-message "sorry, unimplemented: non-rectangular loop nest with step other than constant 1 or -1 for 'j'" }
+  do j = i, 10, step  ! { dg-message "sorry, unimplemented: non-rectangular loop nest with non-constant step for 'j'" }
   end do
 end do
 if (i /= 11) stop 1
