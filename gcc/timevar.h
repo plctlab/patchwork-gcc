4fb255b93cf7903e9a5f0945892a02a6498057fc
Matthew Malcomson <matthew.malcomson@arm.com>
2023-08-03 14:38:32 +0100

    mid-end: Use integral time intervals in timevar.cc
    >
    > I think this is undesriable.  With fused you mean we use FMA?
    > I think you could use -ffp-contract=off for the TU instead.
    >
    > Note you can't use __attribute__((noinline)) literally since the
    > host compiler might not support this.
    >
    > Richard.
    >
    
    Trying to make the timevar store integral time intervals.
    Hope this is acceptable -- I had originally planned to use
    `-ffp-contract` as agreed until I saw the email mentioning the old x86
    bug in the same area which was not to do with floating point contraction
    of operations (PR 99903) and figured it would be better to try and solve
    both at the same time while making things in general a bit more robust.

--- gcc/timevar.h
+++ gcc/timevar.h
@@ -44,7 +44,7 @@
 */
 
 /* This structure stores the various varieties of time that can be
-   measured.  Times are stored in seconds.  The time may be an
+   measured.  Times are stored in nanoseconds.  The time may be an
    absolute time or a time difference; in the former case, the time
    base is undefined, except that the difference between two times
    produces a valid time difference.  */
@@ -52,14 +52,13 @@
 struct timevar_time_def
 {
   /* User time in this process.  */
-  double user;
+  uint64_t user;
 
-  /* System time (if applicable for this host platform) in this
-     process.  */
-  double sys;
+  /* System time (if applicable for this host platform) in this process.  */
+  uint64_t sys;
 
   /* Wall clock time.  */
-  double wall;
+  uint64_t wall;
 
   /* Garbage collector memory.  */
   size_t ggc_mem;
