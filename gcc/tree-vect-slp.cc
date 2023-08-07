9c9ff9418c780bfabbbe7cdfeee3243c19cbd68d
Richard Biener <rguenther@suse.de>
2023-08-07 13:15:52 +0000

    Improve -fopt-info-vec for basic-block vectorization
    We currently dump notes like
    
    flow_lam.f:65:72: optimized: basic block part vectorized using 32 byte vectors
    flow_lam.f:65:72: optimized: basic block part vectorized using 32 byte vectors
    flow_lam.f:65:72: optimized: basic block part vectorized using 32 byte vectors
    flow_lam.f:65:72: optimized: basic block part vectorized using 32 byte vectors
    ..
    
    repeating the same location for multiple instances because we
    clobber vect_location during BB vectorization.  The following
    avoids this, improving things to
    
    flow_lam.f:15:72: optimized: basic block part vectorized using 32 byte vectors
    flow_lam.f:16:72: optimized: basic block part vectorized using 32 byte vectors
    flow_lam.f:17:72: optimized: basic block part vectorized using 32 byte vectors
    flow_lam.f:18:72: optimized: basic block part vectorized using 32 byte vectors
    ...
    
    Bootstrapped and tested on x86_64-unknown-linux-gnu, pushed.
    
    	* tree-vect-slp.cc (vect_slp_region): Save/restore vect_location
    	around dumping code.

--- gcc/tree-vect-slp.cc
+++ gcc/tree-vect-slp.cc
@@ -7435,6 +7435,7 @@ vect_slp_region (vec<basic_block> bbs, vec<data_reference_p> datarefs,
 	      if (instance->subgraph_entries.is_empty ())
 		continue;
 
+	      dump_user_location_t saved_vect_location = vect_location;
 	      vect_location = instance->location ();
 	      if (!unlimited_cost_model (NULL)
 		  && !vect_bb_vectorization_profitable_p
@@ -7444,9 +7445,11 @@ vect_slp_region (vec<basic_block> bbs, vec<data_reference_p> datarefs,
 		    dump_printf_loc (MSG_MISSED_OPTIMIZATION, vect_location,
 				     "not vectorized: vectorization is not "
 				     "profitable.\n");
+		  vect_location = saved_vect_location;
 		  continue;
 		}
 
+	      vect_location = saved_vect_location;
 	      if (!dbg_cnt (vect_slp))
 		continue;
 
@@ -7496,6 +7499,9 @@ vect_slp_region (vec<basic_block> bbs, vec<data_reference_p> datarefs,
 				 "using SLP\n");
 	      vectorized = true;
 
+	      dump_user_location_t saved_vect_location = vect_location;
+	      vect_location = instance->location ();
+
 	      vect_schedule_slp (bb_vinfo, instance->subgraph_entries);
 
 	      unsigned HOST_WIDE_INT bytes;
@@ -7511,6 +7517,8 @@ vect_slp_region (vec<basic_block> bbs, vec<data_reference_p> datarefs,
 				     "basic block part vectorized using "
 				     "variable length vectors\n");
 		}
+
+	      vect_location = saved_vect_location;
 	    }
 	}
       else
