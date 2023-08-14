d1fe6c00cfccdb12898252ec7ebb084326712af2
Tsukasa OI <research_trasio@irq.a4lg.com>
2023-08-14 06:09:51 +0000

    RISC-V: Add stub support for existing extensions (privileged)
    After commit c283c4774d1c ("RISC-V: Throw compilation error for unknown
    extensions") changed how do we handle unknown extensions, we have no
    guarantee that we can share the same architectural string with Binutils
    (specifically, the assembler).
    
    To avoid compilation errors on shared Assembler-C/C++ projects, GCC should
    support almost all extensions that Binutils support, even if the GCC does
    not touch a thing.
    
    As a start, this commit adds stub supported *privileged* extensions to
    riscv_ext_version_table and its implications to riscv_implied_info
    (all information is copied from Binutils' bfd/elfxx-riscv.c).
    
    gcc/ChangeLog:
    
    	* common/config/riscv/riscv-common.cc
    	(riscv_implied_info): Add implications from privileged extensions.
    	(riscv_ext_version_table): Add stub support for all privileged
    	extensions supported by Binutils.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/predef-29.c: New test for a stub privileged
    	extension 'Smstateen' with some implications.

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -108,6 +108,14 @@ static const riscv_implied_info_t riscv_implied_info[] =
   {"zhinx", "zhinxmin"},
   {"zhinxmin", "zfinx"},
 
+  {"smaia", "ssaia"},
+  {"smstateen", "ssstateen"},
+  {"smepmp", "zicsr"},
+  {"ssaia", "zicsr"},
+  {"sscofpmf", "zicsr"},
+  {"ssstateen", "zicsr"},
+  {"sstc", "zicsr"},
+
   {NULL, NULL}
 };
 
@@ -219,8 +227,18 @@ static const struct riscv_ext_version riscv_ext_version_table[] =
 
   {"zmmul", ISA_SPEC_CLASS_NONE, 1, 0},
 
+  {"smaia",     ISA_SPEC_CLASS_NONE, 1, 0},
+  {"smepmp",    ISA_SPEC_CLASS_NONE, 1, 0},
+  {"smstateen", ISA_SPEC_CLASS_NONE, 1, 0},
+
+  {"ssaia",     ISA_SPEC_CLASS_NONE, 1, 0},
+  {"sscofpmf",  ISA_SPEC_CLASS_NONE, 1, 0},
+  {"ssstateen", ISA_SPEC_CLASS_NONE, 1, 0},
+  {"sstc",      ISA_SPEC_CLASS_NONE, 1, 0},
+
   {"svinval", ISA_SPEC_CLASS_NONE, 1, 0},
   {"svnapot", ISA_SPEC_CLASS_NONE, 1, 0},
+  {"svpbmt",  ISA_SPEC_CLASS_NONE, 1, 0},
 
   {"xtheadba", ISA_SPEC_CLASS_NONE, 1, 0},
   {"xtheadbb", ISA_SPEC_CLASS_NONE, 1, 0},
