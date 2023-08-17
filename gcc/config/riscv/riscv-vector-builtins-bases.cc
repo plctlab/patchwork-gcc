e82fd1b18cc2862ab5d3a160d3efe3b05c51a0f6
Pan Li <pan2.li@intel.com>
2023-08-17 15:26:12 +0800

    RISC-V: Support RVV VFWREDOSUM.VS rounding mode intrinsic API
    This patch would like to support the rounding mode API for the
    VFWREDOSUM.VS as the below samples
    
    * __riscv_vfwredosum_vs_f32m1_f64m1_rm
    * __riscv_vfwredosum_vs_f32m1_f64m1_rm_m
    
    Signed-off-by: Pan Li <pan2.li@intel.com>
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-vector-builtins-bases.cc
    	(widen_freducop): Add frm_opt_type template arg.
    	(vfwredosum_frm_obj): New declaration.
    	(BASE): Ditto.
    	* config/riscv/riscv-vector-builtins-bases.h: Ditto.
    	* config/riscv/riscv-vector-builtins-functions.def
    	(vfwredosum_frm): New intrinsic function def.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/base/float-point-wredosum.c: New test.

--- gcc/config/riscv/riscv-vector-builtins-bases.cc
+++ gcc/config/riscv/riscv-vector-builtins-bases.cc
@@ -1417,10 +1417,15 @@ public:
 };
 
 /* Implements widening floating-point reduction instructions.  */
-template<int UNSPEC>
+template<int UNSPEC, enum frm_op_type FRM_OP = NO_FRM>
 class widen_freducop : public function_base
 {
 public:
+  bool has_rounding_mode_operand_p () const override
+  {
+    return FRM_OP == HAS_FRM;
+  }
+
   bool apply_mask_policy_p () const override { return false; }
 
   rtx expand (function_expander &e) const override
@@ -2077,6 +2082,7 @@ static CONSTEXPR const reducop<SMAX> vfredmax_obj;
 static CONSTEXPR const reducop<SMIN> vfredmin_obj;
 static CONSTEXPR const widen_freducop<UNSPEC_UNORDERED> vfwredusum_obj;
 static CONSTEXPR const widen_freducop<UNSPEC_ORDERED> vfwredosum_obj;
+static CONSTEXPR const widen_freducop<UNSPEC_ORDERED, HAS_FRM> vfwredosum_frm_obj;
 static CONSTEXPR const vmv vmv_x_obj;
 static CONSTEXPR const vmv_s vmv_s_obj;
 static CONSTEXPR const vmv vfmv_f_obj;
@@ -2302,6 +2308,7 @@ BASE (vfredosum)
 BASE (vfredmax)
 BASE (vfredmin)
 BASE (vfwredosum)
+BASE (vfwredosum_frm)
 BASE (vfwredusum)
 BASE (vmv_x)
 BASE (vmv_s)
