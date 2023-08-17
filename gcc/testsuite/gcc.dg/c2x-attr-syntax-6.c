8c314fd572813a2aebd1d617b335643da0022df6
Richard Sandiford <richard.sandiford@arm.com>
2023-08-17 12:24:48 +0100

    c: Add support for [[__extension__ ...]]
    Joseph Myers <joseph@codesourcery.com> writes:
    > On Wed, 16 Aug 2023, Richard Sandiford via Gcc-patches wrote:
    >
    >> Would it be OK to add support for:
    >>
    >>   [[__extension__ ...]]
    >>
    >> to suppress the pedwarn about using [[]] prior to C2X?  Then we can
    >
    > That seems like a plausible feature to add.
    
    Thanks.  Of course, once I actually tried it, I hit a snag:
    :: isn't a single lexing token prior to C2X, and so something like:
    
      [[__extension__ arm::streaming]]
    
    would not be interpreted as a scoped attribute in C11.  The patch
    gets around that by allowing two colons in place of :: when
    __extension__ is used.  I realise that's pushing the bounds of
    acceptability though...
    
    I wondered about trying to require the two colons to be immediately
    adjacent.  But:
    
    (a) There didn't appear to be an existing API to check that, which seemed
        like a red flag.  The closest I could find was get_source_text_between.
    
        Similarly to that, it would in principle be possible to compare
        two expanded locations.  But...
    
    (b) I had a vague impression that locations were allowed to drop column
        information for very large inputs (maybe I'm wrong).
    
    (c) It wouldn't cope with token pasting.
    
    So in the end I just used a simple two-token test, like for [[ and ]].
    
    Bootstrapped & regression-tested on aarch64-linux-gnu.
    
    Richard
    
    ----
    
    [[]] attributes are a recent addition to C, but as a GNU extension,
    GCC allows them to be used in C11 and earlier.  Normally this use
    would trigger a pedwarn (for -pedantic, -Wc11-c2x-compat, etc.).
    
    This patch allows the pedwarn to be suppressed by starting the
    attribute-list with __extension__.
    
    Also, :: is not a single lexing token prior to C2X, so it wasn't
    possible to use scoped attributes in C11, even as a GNU extension.
    The patch allows two colons to be used in place of :: when
    __extension__ is used.  No attempt is made to check whether the
    two colons are immediately adjacent.
    
    gcc/
    	* doc/extend.texi: Document the C [[__extension__ ...]] construct.
    
    gcc/c/
    	* c-parser.cc (c_parser_std_attribute): Conditionally allow
    	two colons to be used in place of ::.
    	(c_parser_std_attribute_list): New function, split out from...
    	(c_parser_std_attribute_specifier): ...here.  Allow the attribute-list
    	to start with __extension__.  When it does, also allow two colons
    	to be used in place of ::.
    
    gcc/testsuite/
    	* gcc.dg/c2x-attr-syntax-6.c: New test.
    	* gcc.dg/c2x-attr-syntax-7.c: Likewise.

---
+++ gcc/testsuite/gcc.dg/c2x-attr-syntax-6.c
@@ -0,0 +1,50 @@
+/* Test C2x attribute syntax: use of __extension__ in C11 mode.  */
+/* { dg-do compile } */
+/* { dg-options "-std=c11 -pedantic-errors" } */
+
+#define FOO ::
+#define BAR :
+
+typedef int [[__extension__ gnu :: vector_size (4)]] g1;
+typedef int [[__extension__ gnu : : vector_size (4)]] g2;
+typedef int [[__extension__ gnu FOO vector_size (4)]] g3;
+typedef int [[__extension__ gnu BAR BAR vector_size (4)]] g4;
+typedef int [[__extension__ gnu :: vector_size (sizeof (void (*)(...)))]] g5;
+typedef int [[__extension__]] g6;
+typedef int [[__extension__,]] g7;
+typedef int [[__extension__, ,,,, ,, ,]] g8;
+[[__extension__ deprecated]] int g9 ();
+[[__extension__ nodiscard]] int g10 ();
+[[__extension__ noreturn]] void g11 ();
+
+int
+cases (int x)
+{
+  switch (x)
+    {
+    case 1:
+    case 2:
+    case 4:
+      x += 1;
+      [[__extension__ fallthrough]];
+    case 19:
+    case 33:
+      x *= 2;
+      [[fallthrough]];  /* { dg-error {attributes before C2X} } */
+    case 99:
+      return x;
+
+    default:
+      return 0;
+    }
+}
+
+typedef int [[__extension__ vector_size (4)]] b1; /* { dg-error {'vector_size' attribute ignored} } */
+typedef int [[__extension__ __extension__]] b2; /* { dg-error {'extension' attribute ignored} } */
+typedef int [[__extension__ unknown_attribute]] b3; /* { dg-error {'unknown_attribute' attribute ignored} } */
+typedef int [[gnu::vector_size(4)]] b4; /* { dg-error {expected '\]' before ':'} } */
+/* { dg-error {'gnu' attribute ignored} "" { target *-*-* } .-1 } */
+/* { dg-error {attributes before C2X} "" { target *-*-* } .-2 } */
+typedef int [[gnu : : vector_size(4)]] b5; /* { dg-error {expected '\]' before ':'} } */
+/* { dg-error {'gnu' attribute ignored} "" { target *-*-* } .-1 } */
+/* { dg-error {attributes before C2X} "" { target *-*-* } .-2 } */
