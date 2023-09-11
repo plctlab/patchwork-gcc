1409f12f1a8334a5f2f4ea14a27d7b6be152558e
Pan Li <pan2.li@intel.com>
2023-09-11 15:57:27 +0800

    RISC-V: Implement RESOLVE_OVERLOADED_BUILTIN for RVV intrinsic
    This patch would like add the framework to support the RVV overloaded
    intrinsic API in riscv-xxx-xxx-gcc, like riscv-xxx-xxx-g++ did.
    
    However, it almost leverage the hook TARGET_RESOLVE_OVERLOADED_BUILTIN
    with below steps.
    
    * Register overloaded functions.
    * Add function_resolver for overloaded function resolving.
    * Add resolve API for function shape with default implementation.
    * Implement HOOK for navigating the overloaded API to non-overloaded API.
    
    We validated this framework by the vmv_v intrinsic API(s), and we will
    add more intrins API support in the underlying patches.
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-c.cc
    	(riscv_resolve_overloaded_builtin): New function for the hook.
    	(riscv_register_pragmas): Register the hook
    	* config/riscv/riscv-protos.h (resolve_overloaded_builtin): New decl.
    	* config/riscv/riscv-vector-builtins-shapes.cc (build_one):
    	Register overloaded function.
    	(struct overloaded_base): New struct for overloaded shape.
    	(struct non_overloaded_base): New struct for non overloaded shape.
    	(struct move_def): Inherit overloaded shape.
    	* config/riscv/riscv-vector-builtins.cc
    	(function_builder::add_function): Add overloaded arg.
    	(function_builder::add_overloaded_function): New function impl.
    	(function_resolver::function_resolver): New constructor.
    	(function_resolver::get_sub_code): New API impl.
    	(function_resolver::resolve): New API impl.
    	(function_resolver::lookup): New API impl.
    	(resolve_overloaded_builtin): New func impl.
    	* config/riscv/riscv-vector-builtins.h
    	(class function_resolver): New class.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/base/overloaded_rv32_vmv_v.c: New test.
    	* gcc.target/riscv/rvv/base/overloaded_rv64_vmv_v.c: New test.
    	* gcc.target/riscv/rvv/base/overloaded_vmv_v.h: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>
    Signed-off-by: Pan Li <pan2.li@intel.com>

---
+++ gcc/testsuite/gcc.target/riscv/rvv/base/overloaded_rv32_vmv_v.c
@@ -0,0 +1,4 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32gcv_zvfh -mabi=ilp32 -O3 -Wno-psabi" } */
+
+#include "overloaded_vmv_v.h"
---
+++ gcc/testsuite/gcc.target/riscv/rvv/base/overloaded_rv64_vmv_v.c
@@ -0,0 +1,4 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv64gcv_zvfh -mabi=lp64 -O3 -Wno-psabi" } */
+
+#include "overloaded_vmv_v.h"
