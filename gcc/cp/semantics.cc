bd530c5e6cf96d3ae708a7c730d5edd25ead5629
Ken Matsui <kmatsui@gcc.gnu.org>
2023-09-11 17:16:05 -0700

    c++: Implement __is_scoped_enum built-in trait
    This patch implements built-in trait for
    std::is_scoped_enum.
    
    gcc/cp/ChangeLog:
    
    	* cp-trait.def (IS_SCOPED_ENUM): Define
    	__is_scoped_enum.
    	* constraint.cc (diagnose_trait_expr): Handle
    	CPTK_IS_SCOPED_ENUM.
    	* semantics.cc (trait_expr_value): Likewise.
    	(finish_trait_expr): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/ext/has-builtin-1.C: Test existence of
    	is_scoped_enum.
    	* g++.dg/ext/is_scoped_enum.C: New test.
    
    Signed-off-by: Ken Matsui <kmatsui@gcc.gnu.org>

--- gcc/cp/semantics.cc
+++ gcc/cp/semantics.cc
@@ -12043,6 +12043,9 @@ trait_expr_value (cp_trait_kind kind, tree type1, tree type2)
     case CPTK_IS_SAME:
       return same_type_p (type1, type2);
 
+    case CPTK_IS_SCOPED_ENUM:
+      return SCOPED_ENUM_P (type1);
+
     case CPTK_IS_STD_LAYOUT:
       return std_layout_type_p (type1);
 
@@ -12239,6 +12242,7 @@ finish_trait_expr (location_t loc, cp_trait_kind kind, tree type1, tree type2)
     case CPTK_IS_ENUM:
     case CPTK_IS_UNION:
     case CPTK_IS_SAME:
+    case CPTK_IS_SCOPED_ENUM:
       break;
 
     case CPTK_IS_LAYOUT_COMPATIBLE:
