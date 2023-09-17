fa3f8b2b85897105b8e6eec1fce390d90a7341f1
Nathaniel Shead <nathanieloshead@gmail.com>
2023-09-17 22:46:14 +1000

    c++: Catch indirect change of active union member in constexpr [PR101631]
    Ping for https://gcc.gnu.org/pipermail/gcc-patches/2023-September/629084.html
    
    -- >8 --
    
    This patch adds checks for attempting to change the active member of a
    union by methods other than a member access expression.
    
    To be able to properly distinguish `*(&u.a) = ` from `u.a = `, this
    patch redoes the solution for c++/59950 to avoid extranneous *&; it
    seems that the only case that needed the workaround was when copying
    empty classes.
    
    Additionally, this patch ensures that constructors for a union field
    mark that field as the active member before entering the call itself;
    this ensures that modifications of the field within the constructor's
    body don't cause false positives (as these will not appear to be member
    access expressions). This means that we no longer need to start the
    lifetime of empty union members after the constructor body completes.
    
    	PR c++/101631
    
    gcc/cp/ChangeLog:
    
    	* call.cc (build_over_call): Fold more indirect refs for trivial
    	assignment op.
    	* class.cc (type_has_non_deleted_trivial_default_ctor): Create.
    	* constexpr.cc (cxx_eval_call_expression): Start lifetime of
    	union member before entering constructor.
    	(cxx_eval_store_expression): Check for accessing inactive union
    	member indirectly.
    	* cp-tree.h (type_has_non_deleted_trivial_default_ctor):
    	Forward declare.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp2a/constexpr-union2.C: New test.
    	* g++.dg/cpp2a/constexpr-union3.C: New test.
    	* g++.dg/cpp2a/constexpr-union4.C: New test.
    	* g++.dg/cpp2a/constexpr-union5.C: New test.
    
    Signed-off-by: Nathaniel Shead <nathanieloshead@gmail.com>

---
+++ gcc/testsuite/g++.dg/cpp2a/constexpr-union5.C
@@ -0,0 +1,55 @@
+// { dg-do compile { target c++20 } }
+
+union U { int a; int b; int c[2]; };
+
+constexpr int test1() {
+  U u;
+  u.a = 10;
+  *&u.b = 20;  // { dg-error "accessing" }
+  return u.b;
+}
+constexpr int a = test1();  // { dg-message "in .constexpr. expansion" }
+
+constexpr int test2() {
+  U u;
+  u.a = 10;
+  (0, u.b) = 20;  // { dg-error "accessing" }
+  return u.b;
+}
+constexpr int b = test2();  // { dg-message "in .constexpr. expansion" }
+
+constexpr int test3() {
+  U u;
+  u.a = 0;
+  int* p = &u.b;
+  p[u.a] = 10;  // { dg-error "accessing" }
+  return u.b;
+}
+constexpr int c = test3();  // { dg-message "in .constexpr. expansion" }
+
+constexpr int test4() {
+  U u;
+  u.a = 0;
+  int* p = &u.b;
+  u.a[p] = 10;  // { dg-error "accessing" }
+  return u.b;
+}
+constexpr int d = test4();  // { dg-message "in .constexpr. expansion" }
+
+struct S { U u[10]; };
+constexpr int test5() {
+  S s;
+  s.u[4].a = 10;
+  6[s.u].b = 15;
+  return 4[s.u].a + s.u[6].b;
+}
+static_assert(test5() == 25);
+
+constexpr int test6() {
+  U u;
+  u.a = 5;
+  u.c[0] = 3;
+  1[u.c] = 8;
+  return 1[u.c] + u.c[0];
+}
+static_assert(test6() == 11);
