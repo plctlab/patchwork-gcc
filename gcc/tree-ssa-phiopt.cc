fe95029329cdd90b3137cb08e308be25343f3ce1
Andrew Pinski <apinski@marvell.com>
2023-10-01 13:23:15 -0700

    Fix PR 111331: wrong code for `a > 28 ? MIN<a, 28> : 29`
    The problem here is after r6-7425-ga9fee7cdc3c62d0e51730,
    the comparison to see if the transformation could be done was using the
    wrong value. Instead of see if the inner was LE (for MIN and GE for MAX)
    the outer value, it was comparing the inner to the value used in the comparison
    which was wrong.
    
    Committed to GCC 13 branch after bootstrapped and tested on x86_64-linux-gnu.
    
    gcc/ChangeLog:
    
    	PR tree-optimization/111331
    	* tree-ssa-phiopt.cc (minmax_replacement):
    	Fix the LE/GE comparison for the
    	`(a CMP CST1) ? max<a,CST2> : a` optimization.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/111331
    	* gcc.c-torture/execute/pr111331-1.c: New test.
    	* gcc.c-torture/execute/pr111331-2.c: New test.
    	* gcc.c-torture/execute/pr111331-3.c: New test.
    
    (cherry picked from commit 30e6ee074588bacefd2dfe745b188bb20c81fe5e)

--- gcc/tree-ssa-phiopt.cc
+++ gcc/tree-ssa-phiopt.cc
@@ -1977,7 +1977,7 @@ minmax_replacement (basic_block cond_bb, basic_block middle_bb, basic_block alt_
 
 	      /* We need BOUND <= LARGER.  */
 	      if (!integer_nonzerop (fold_build2 (LE_EXPR, boolean_type_node,
-						  bound, larger)))
+						  bound, arg_false)))
 		return false;
 	    }
 	  else if (operand_equal_for_phi_arg_p (arg_false, smaller)
@@ -2008,7 +2008,7 @@ minmax_replacement (basic_block cond_bb, basic_block middle_bb, basic_block alt_
 
 	      /* We need BOUND >= SMALLER.  */
 	      if (!integer_nonzerop (fold_build2 (GE_EXPR, boolean_type_node,
-						  bound, smaller)))
+						  bound, arg_false)))
 		return false;
 	    }
 	  else
@@ -2048,7 +2048,7 @@ minmax_replacement (basic_block cond_bb, basic_block middle_bb, basic_block alt_
 
 	      /* We need BOUND >= LARGER.  */
 	      if (!integer_nonzerop (fold_build2 (GE_EXPR, boolean_type_node,
-						  bound, larger)))
+						  bound, arg_true)))
 		return false;
 	    }
 	  else if (operand_equal_for_phi_arg_p (arg_true, smaller)
@@ -2075,7 +2075,7 @@ minmax_replacement (basic_block cond_bb, basic_block middle_bb, basic_block alt_
 
 	      /* We need BOUND <= SMALLER.  */
 	      if (!integer_nonzerop (fold_build2 (LE_EXPR, boolean_type_node,
-						  bound, smaller)))
+						  bound, arg_true)))
 		return false;
 	    }
 	  else
