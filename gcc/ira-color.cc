5a34a0f29eedc7f229f63a09baaaa5f1144af2b1
Surya Kumari Jangala <jskumari@linux.vnet.ibm.com>
2023-10-03 19:37:25 +0530

    ira: Scale save/restore costs of callee save registers with block frequency
    ira: Scale save/restore costs of callee save registers with block frequency
    
    In assign_hard_reg(), when computing the costs of the hard registers, the
    cost of saving/restoring a callee-save hard register in prolog/epilog is
    taken into consideration. However, this cost is not scaled with the entry
    block frequency. Without scaling, the cost of saving/restoring is quite
    small and this can result in a callee-save register being chosen by
    assign_hard_reg() even though there are free caller-save registers
    available. Assigning a callee save register to a pseudo that is live
    in the entire function and across a call will cause shrink wrap to fail.
    
    2023-10-03  Surya Kumari Jangala  <jskumari@linux.ibm.com>
    
    gcc/
    	PR rtl-optimization/111673
    	* ira-color.cc (assign_hard_reg): Scale save/restore costs of
    	callee save registers with block frequency.
    
    gcc/testsuite/
    	PR rtl-optimization/111673
    	* gcc.target/powerpc/pr111673/c: New test.

--- gcc/ira-color.cc
+++ gcc/ira-color.cc
@@ -2175,7 +2175,8 @@ assign_hard_reg (ira_allocno_t a, bool retry_p)
 	    add_cost = ((ira_memory_move_cost[mode][rclass][0]
 		         + ira_memory_move_cost[mode][rclass][1])
 		        * saved_nregs / hard_regno_nregs (hard_regno,
-							  mode) - 1);
+							  mode) - 1)
+			* REG_FREQ_FROM_BB (ENTRY_BLOCK_PTR_FOR_FN (cfun));
 	    cost += add_cost;
 	    full_cost += add_cost;
 	  }
