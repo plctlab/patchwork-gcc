c778373dcb7b5870f24f367e6feff3f106f2eaba
Patrick O'Neill <patrick@rivosinc.com>
2023-10-04 14:57:42 -0700

    RISC-V: xfail gcc.dg/pr90263.c for riscv_v
    Since r14-4358-g9464e72bcc9 riscv_v targets use vector instructions to
    perform a memcpy. We no longer expect memcpy for riscv_v targets.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/pr90263.c: xfail riscv_v targets.
    
    Signed-off-by: Patrick O'Neill <patrick@rivosinc.com>
    Co-authored-by: Joern Rennecke <joern.rennecke@embecosm.com>

--- gcc/testsuite/gcc.dg/pr90263.c
+++ gcc/testsuite/gcc.dg/pr90263.c
@@ -9,4 +9,4 @@ int *f (int *p, int *q, long n)
 }
 
 /* { dg-final { scan-assembler "mempcpy" { target { i?86-*-* x86_64-*-* } } } } */
-/* { dg-final { scan-assembler "memcpy" { target { ! { i?86-*-* x86_64-*-* } } } } } */
+/* { dg-final { scan-assembler "memcpy" { target { ! { i?86-*-* x86_64-*-* } } xfail { riscv_v } } } } */
