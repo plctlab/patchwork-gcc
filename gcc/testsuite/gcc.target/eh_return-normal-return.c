fd7d5c7ae14a4b2b95bcb05681faaad315b01db0
Yang Yujie <yangyujie@loongson.cn>
2023-12-06 14:44:16 +0800

    LoongArch: Fix eh_return epilogue for normal returns
    On LoongArch, the regitsters $r4 - $r7 (EH_RETURN_DATA_REGNO) will be saved
    and restored in the function prologue and epilogue if the given function calls
    __builtin_eh_return.  This causes the return value to be overwritten on normal
    return paths and breaks a rare case of libgcc's _Unwind_RaiseException.
    
    gcc/ChangeLog:
    
    	* config/loongarch/loongarch.cc: Do not restore the saved eh_return
    	data registers ($r4-$r7) for a normal return of a function that calls
    	__builtin_eh_return elsewhere.
    	* config/loongarch/loongarch-protos.h: Same.
    	* config/loongarch/loongarch.md: Same.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/eh_return-normal-return.c: New test.

---
+++ gcc/testsuite/gcc.target/eh_return-normal-return.c
@@ -0,0 +1,31 @@
+/* { dg-do run } */
+/* { dg-options "-O2" } */
+
+#include <stdlib.h>
+
+int foo()  __attribute__((noinline));
+int main (void);
+
+int foo ()
+{
+    int t;
+
+    /* prevent optimization using asm */
+    asm ("" : "=r" (t) : "0" (-1));
+    asm ("" : "=r" (t) : "0" (t ? 1 : 0));
+
+    if (t == 0)
+        /* never reached */
+        __builtin_eh_return (0, __builtin_return_address (0));
+    else
+        return 202312;
+}
+
+
+int main (void)
+{
+   if (foo() == 202312)
+	  return 0; 
+   else
+	  abort ();
+}
