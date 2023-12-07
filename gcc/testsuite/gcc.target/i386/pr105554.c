3035f4e45a9f173a72486d7fab1e1d2c5b2719f9
Marc Poulhiès <poulhies@adacore.com>
2023-12-07 17:25:39 +0100

    testsuite: add missing dg-require ifunc in pr105554.c
    The 'target_clones' attribute depends on the ifunc support.
    
    gcc/testsuite/ChangeLog:
    	* gcc.target/i386/pr105554.c: Add dg-require ifunc.

--- gcc/testsuite/gcc.target/i386/pr105554.c
+++ gcc/testsuite/gcc.target/i386/pr105554.c
@@ -1,6 +1,7 @@
 /* PR target/105554 */
 /* { dg-do compile } */
 /* { dg-options "-O2 -Wno-psabi -mno-sse3" } */
+/* { dg-require-ifunc "" } */
 
 typedef long long v4di __attribute__((__vector_size__(32)));
 
