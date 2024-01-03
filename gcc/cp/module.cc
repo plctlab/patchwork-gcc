c4a88cf19538d743f2336f3667e7a628c0763f8a
Nathaniel Shead <nathanieloshead@gmail.com>
2024-01-03 23:42:43 +1100

    c++/modules: Emit definitions of ODR-used static members imported from modules [PR112899]
    Linaro CI tells me that this patch caused regressions on ARM. I don't
    have an ARM machine available to test on, but it appears to have been
    caused by attempting to stream vtables as static data members, and ARM
    having different behaviour with regards to when DECL_INTERFACE_KNOWN is
    marked on vtables.
    
    I don't think treating vtables as static members here is desirable
    anyway, though, so this version of the patch explicitly ignores them.
    
    Manually checking the assembly output for some of the noted regressions
    on ARM with a cross-compiler, this seems to have worked. Otherwise
    bootstrapped and regtested on x86_64-pc-linux-gnu, OK for trunk if it
    passes Linaro CI on ARM this time?
    
    -- >8 --
    
    Static data members marked 'inline' should be emitted in TUs where they
    are ODR-used.  We need to make sure that statics imported from modules
    are correctly added to the 'pending_statics' map so that they get
    emitted if needed, otherwise the attached testcase fails to link.
    
    	PR c++/112899
    
    gcc/cp/ChangeLog:
    
    	* cp-tree.h (note_variable_template_instantiation): Rename to...
    	(note_static_storage_variable): ...this.
    	* decl2.cc (note_variable_template_instantiation): Rename to...
    	(note_static_storage_variable): ...this.
    	* pt.cc (instantiate_decl): Rename usage of above function.
    	* module.cc (trees_in::read_var_def): Remember pending statics
    	that we stream in.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/modules/init-4_a.C: New test.
    	* g++.dg/modules/init-4_b.C: New test.
    
    Signed-off-by: Nathaniel Shead <nathanieloshead@gmail.com>

--- gcc/cp/module.cc
+++ gcc/cp/module.cc
@@ -11724,6 +11724,11 @@ trees_in::read_var_def (tree decl, tree maybe_template)
 	  DECL_INITIALIZED_P (decl) = true;
 	  if (maybe_dup && DECL_INITIALIZED_BY_CONSTANT_EXPRESSION_P (maybe_dup))
 	    DECL_INITIALIZED_BY_CONSTANT_EXPRESSION_P (decl) = true;
+	  if (DECL_CONTEXT (decl)
+	      && RECORD_OR_UNION_TYPE_P (DECL_CONTEXT (decl))
+	      && !DECL_TEMPLATE_INFO (decl)
+	      && !DECL_VTABLE_OR_VTT_P (decl))
+	    note_static_storage_variable (decl);
 	}
       DECL_INITIAL (decl) = init;
       if (!dyn_init)
