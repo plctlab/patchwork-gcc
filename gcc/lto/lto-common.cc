9d50e4304c9efa0215600797157db63626704cd5
Michal Jires <mjires@suse.cz>
2024-01-09 17:49:34 +0100

    lto: Remove random_seed from section name.
    This patch removes suffixes from section names during LTO linking.
    
    These suffixes were originally added for ld -r to work (PR lto/44992).
    They were added to all LTO object files, but are only useful before WPA.
    After that they waste space, and if kept random, make LTO caching impossible.
    
    Bootstrapped/regtested on x86_64-pc-linux-gnu
    
    gcc/ChangeLog:
    
    	* lto-streamer.cc (lto_get_section_name): Remove suffixes after WPA.
    
    gcc/lto/ChangeLog:
    
    	* lto-common.cc (lto_section_with_id): Dont load suffix during LTRANS.

--- gcc/lto/lto-common.cc
+++ gcc/lto/lto-common.cc
@@ -2173,6 +2173,13 @@ lto_section_with_id (const char *name, unsigned HOST_WIDE_INT *id)
 
   if (strncmp (name, section_name_prefix, strlen (section_name_prefix)))
     return 0;
+
+  if (flag_ltrans)
+    {
+      *id = 0;
+      return 1;
+    }
+
   s = strrchr (name, '.');
   if (!s)
     return 0;
