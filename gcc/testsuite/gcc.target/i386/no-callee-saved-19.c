c8f47a8b452536d65caa43bee8b5647861d035df
H.J. Lu <hjl.tools@gmail.com>
2024-01-29 13:55:18 -0800

    x86: Generate REG_CFA_UNDEFINED for unsaved callee-saved registers
    Changes in v3:
    
    1. Fix a typo in REG_CFA_UNDEFINED note comment.
    2. Replace assemble with compile in tests and remove -save-temps since
    ".cfi_undefined regno" is generated now.
    
    Changes in v2:
    
    1. Add REG_CFA_UNDEFINED notes to a frame-related instruction in prologue.
    2. Add comments for add_cfi_undefined.

---
+++ gcc/testsuite/gcc.target/i386/no-callee-saved-19.c
@@ -0,0 +1,17 @@
+/* { dg-do compile { target *-*-linux* *-*-gnu* } } */
+/* { dg-options "-march=tigerlake -O2 -mtune-ctrl=^prologue_using_move,^epilogue_using_move" } */
+
+#include "no-callee-saved-1.c"
+
+/* { dg-final { scan-assembler-not "push" } } */
+/* { dg-final { scan-assembler-not "pop" } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 3" 1 { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 6" 1 { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 12" 1 { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 13" 1 { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 14" 1 { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 15" 1 { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 3" 1 { target ia32 } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 5" 1 { target ia32 } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 6" 1 { target ia32 } } } */
+/* { dg-final { scan-assembler-times ".cfi_undefined 7" 1 { target ia32 } } } */
