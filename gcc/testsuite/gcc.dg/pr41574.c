f75dd7b3e08e284ce45c9a34709e7432bed93aac
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2024-02-13 11:44:09 +0100

    testsuite: Disable test for incompatible Arm targets
    Ok for trunk and releases/gcc-13?
    
    The alternative approach (that is changing the result a bit) is to drop
    the special treatment for arm*-*-*. I'm not sure if this is prefered or
    just disable the test for incompatible flags for arm*-*-*.

--- gcc/testsuite/gcc.dg/pr41574.c
+++ gcc/testsuite/gcc.dg/pr41574.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
-/* { dg-options "-O2 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fno-unsafe-math-optimizations -fdump-rtl-combine" { target { arm*-*-* } } } */
-/* { dg-options "-O2 -fno-unsafe-math-optimizations -fdump-rtl-combine" { target { ! arm*-*-* } } } */
+/* { dg-options "-O2 -fno-unsafe-math-optimizations -fdump-rtl-combine" } */
+/* { dg-require-effective-target arm_arch_v7a_neon_multilib { target { arm*-*-* } } } */
+/* { dg-additional-options "-march=armv7-a -mfloat-abi=softfp -mfpu=neon" { target { arm*-*-* } } } */
 
 
 static const double one=1.0;
