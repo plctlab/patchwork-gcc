1484daab6a4706d2e349570b2d77328d36c71d1b
Stefan Schulze Frielinghaus <stefansf@linux.ibm.com>
2024-02-29 13:15:04 +0100

    s390: Fix test vector/long-double-to-i64.c
    Starting with r14-8319-g86de9b66480b71 fwprop improved so that vpdi is
    no longer required.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/s390/vector/long-double-to-i64.c: Fix scan
    	assembler directive.

--- gcc/testsuite/gcc.target/s390/vector/long-double-to-i64.c
+++ gcc/testsuite/gcc.target/s390/vector/long-double-to-i64.c
@@ -1,18 +1,23 @@
 /* { dg-do compile } */
 /* { dg-options "-O3 -march=z14 -mzarch --save-temps" } */
 /* { dg-do run { target { s390_z14_hw } } } */
+/* { dg-final { check-function-bodies "**" "" "" { target { lp64 } } } } */
+
 #include <assert.h>
 #include <stdint.h>
 
+/*
+** long_double_to_i64:
+**	ld	%f0,0\(%r2\)
+**	ld	%f2,8\(%r2\)
+**	cgxbr	%r2,5,%f0
+**	br	%r14
+*/
 __attribute__ ((noipa)) static int64_t
 long_double_to_i64 (long double x)
 {
   return x;
 }
-
-/* { dg-final { scan-assembler-times {\n\tvpdi\t%v\d+,%v\d+,%v\d+,1\n} 1 } } */
-/* { dg-final { scan-assembler-times {\n\tvpdi\t%v\d+,%v\d+,%v\d+,5\n} 1 } } */
-/* { dg-final { scan-assembler-times {\n\tcgxbr\t} 1 } } */
 
 int
 main (void)
